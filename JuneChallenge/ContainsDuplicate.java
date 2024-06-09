0f1ad482562689b6283aaf4f864e1f9f5078a94a
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2024-06-09 11:58:36 +0530

    217. [Easy] Contains Duplicate

---
+++ JuneChallenge/ContainsDuplicate.java
@@ -0,0 +1,23 @@
+package JuneChallenge;
+
+import java.util.HashSet;
+import java.util.Set;
+
+// 217. [Easy] Contains Duplicate
+// 09 June 2024
+public class ContainsDuplicate {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,1};
+        System.out.println(containsDuplicate(nums));
+    }
+    public static boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<Integer>();
+
+        for(int i : nums){
+            set.add(i);
+        }
+
+        
+        return (nums.length != set.size());
+    }
+}
