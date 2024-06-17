2c30777824198a161c098b746fea2bcff6fea91f
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2024-06-17 18:41:16 +0530

    Print all diagonals

--- Intermediate/Matrix/Diagonals.java
+++ Intermediate/Matrix/Diagonals.java
@@ -1,5 +1,8 @@
 package Matrix;
 
+// Intermediate 1 _ 2D Array
+// Print Both diagonals
+// Print all diagonals from one matrix
 public class Diagonals {
     public static void main(String[] args) {
         int[][] mat = {
@@ -9,6 +12,7 @@ public class Diagonals {
         };
 
         printDiagonals(mat);
+        printDiagonalsRightToLeft(mat);
 
     }
     public static void printDiagonals(int[][] mat){
@@ -28,6 +32,38 @@ public class Diagonals {
 
         for(int i = 0, j = n-1; i < n; i++,j--){
             System.out.print( mat[i][j] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void printDiagonalsRightToLeft(int[][] mat){
+        int n = mat.length;
+        int m = mat[0].length;
+
+        //Print all diagonals starting from 0th row
+        for(int c = 0; c < m; c++){
+            int  i = 0;
+            int  j = c;
+
+            while(i < n && j >= 0){
+                System.out.print(mat[i][j] + " ");
+                i++;
+                j--;
+            }
+            System.out.println();
+        }
+
+        //Print all diagonals starting from m-1 col
+        for(int r = 1; r < m; r++){
+            int  i = r;
+            int  j = m-1;
+
+            while(i < n && j >= 0){
+                System.out.print(mat[i][j] + " ");
+                i++;
+                j--;
+            }
+            System.out.println();
         }
     }
 
