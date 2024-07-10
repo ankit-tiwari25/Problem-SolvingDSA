c9da05ee69cf64892030bc6441cf7e6c289da751
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2024-07-10 18:34:36 +0530

    1598. [Easy] Crawler Log Folder

---
+++ JulyChallenge/JulyChallenge/CrawlerLogFolder.java
@@ -0,0 +1,27 @@
+package JulyChallenge;
+
+import java.util.Stack;
+// 1598. [Easy] Crawler Log Folder
+//Stack
+public class CrawlerLogFolder {
+    public static void main(String[] args) {
+        String[] logs = {"d1/","d2/","../","d21/","./"};
+
+        System.out.println(minOperations(logs));
+    }
+    public static int minOperations(String[] logs) {
+        Stack<String> paths_stack = new Stack<>();
+
+       for (String log : logs) {
+           if (log.equals("../")) {
+               if (!paths_stack.isEmpty()) {
+                   paths_stack.pop();
+               }
+           } else if (!log.equals("./")) {
+               paths_stack.push(log);
+           }
+       }
+
+       return paths_stack.size();
+   }
+}
