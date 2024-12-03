6eaaab9b007289bf666e9ff299bc998374484828
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2024-12-03 14:46:49 +0530

    2109. [Medium] Adding Spaces to a String

---
+++ DecemberChallenge/December3Problem2109AddingSpacesToAString.java
@@ -0,0 +1,34 @@
+package DecemberChallenge;
+
+/*
+ * 2109. [Medium] Adding Spaces to a String
+ */
+public class December3Problem2109AddingSpacesToAString {
+    public static void main(String[] args) {
+        // String s = "EnjoyYourCoffee";
+        // int[] space = {5,9};
+        String s = "LeetcodeHelpsMeLearn";
+        int[] spaces = {8,13,15};
+
+        System.out.println(addSpaces(s, spaces));
+    }
+    public static String addSpaces(String s, int[] spaces) {
+        StringBuilder str = new StringBuilder();
+        int n = s.length();
+        for(int i = 0, j = 0; i < n; i++){
+            if(j < spaces.length){
+                int idx = spaces[j];
+            if(i == idx){
+                str.append(" ");
+                j++;
+            }
+            }
+            
+            str.append(""+s.charAt(i));
+            
+            // System.out.println(str);
+
+        }
+        return str.toString();
+    }
+}
