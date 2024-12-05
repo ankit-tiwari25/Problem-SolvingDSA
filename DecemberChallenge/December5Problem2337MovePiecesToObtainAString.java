7c0e3691b2ea35957bcec40650032ff3f277b90d
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2024-12-05 23:12:48 +0530

    2337. [Medium] Move Pieces to Obtain a String

---
+++ DecemberChallenge/December5Problem2337MovePiecesToObtainAString.java
@@ -0,0 +1,24 @@
+package DecemberChallenge;
+/* [Interesting Question : Two Pointer]
+ * 2337. [Medium] Move Pieces to Obtain a String
+ */
+public class December5Problem2337MovePiecesToObtainAString {
+    public static void main(String[] args) {
+        
+    }
+    public static boolean canChange(String start, String target) {
+        int n = start.length();
+        int i = 0;
+        int j = 0;
+        int lastIndex = -1;
+        skipSpaces(start, i, lastIndex)
+        while()
+        return true;
+    }
+    public static int skipSpaces(String str, int pos, int n){
+        while(str.charAt(pos) == '_'){
+            pos++;
+        }
+        return pos;
+    }
+}
