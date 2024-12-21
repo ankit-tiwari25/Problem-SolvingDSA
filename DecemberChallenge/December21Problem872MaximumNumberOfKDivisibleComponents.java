7061f40c30c0340cd9bbdaa851b1ce129aa2add3
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2024-12-21 23:07:33 +0530

    [Hard] Maximum Number of K-Divisible Components

---
+++ DecemberChallenge/December21Problem872MaximumNumberOfKDivisibleComponents.java
@@ -0,0 +1,7 @@
+package DecemberChallenge;
+/* 872. [Hard] Maximum Number of K-Divisible Components */
+public class December21Problem872MaximumNumberOfKDivisibleComponents {
+    public static void main(String[] args) {
+        
+    }
+}
