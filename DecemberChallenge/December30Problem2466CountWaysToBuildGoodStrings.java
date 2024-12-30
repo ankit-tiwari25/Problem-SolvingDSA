611096e1f7a977ed411204491ed89c431850c0ab
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2024-12-30 09:13:58 +0530

    2466. [Medium] Count Ways To Build Good Strings

---
+++ DecemberChallenge/December30Problem2466CountWaysToBuildGoodStrings.java
@@ -0,0 +1,9 @@
+package DecemberChallenge;
+/*
+ *  2466. [Medium] Count Ways To Build Good Strings
+ */
+public class December30Problem2466CountWaysToBuildGoodStrings {
+    public static void main(String[] args) {
+        
+    }
+}
