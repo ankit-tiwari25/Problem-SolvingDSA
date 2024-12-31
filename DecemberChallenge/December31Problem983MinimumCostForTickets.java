d5dbd455f8676bfec6d8be514a4d6038552977b2
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2024-12-31 16:00:36 +0530

    983.[Medium] Minimum Cost For Tickets

---
+++ DecemberChallenge/December31Problem983MinimumCostForTickets.java
@@ -0,0 +1,9 @@
+package DecemberChallenge;
+/*
+ * 983.[Medium] Minimum Cost For Tickets
+ */
+public class December31Problem983MinimumCostForTickets {
+    public static void main(String[] args) {
+        
+    }
+}
