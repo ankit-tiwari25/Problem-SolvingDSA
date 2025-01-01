9f70eeab17a5a9c2e99f77d84d8c9f4b363e2290
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-01-01 17:04:52 +0530

    1422.[Easy] Maximum Score After Splitting a String

---
+++ January2025Challenge/Jan1Problem1422MaximumScoreAfterSplittingaString.java
@@ -0,0 +1,10 @@
+package January2025Challenge;
+
+/*
+ *  1422.[Easy] Maximum Score After Splitting a String
+ */
+public class Jan1Problem1422MaximumScoreAfterSplittingaString {
+    public static void main(String[] args) {
+        
+    }
+}
