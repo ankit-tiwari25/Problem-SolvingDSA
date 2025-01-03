71eee3ed4822b9211111c19f99bf8c139aa83167
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-01-03 19:58:08 +0530

    2270.[Medium] Number of Ways to Split Array

--- January2025Challenge/Jan3Problem2270NumberofWaystoSplitArray.java
+++ January2025Challenge/Jan3Problem2270NumberofWaystoSplitArray.java
@@ -5,4 +5,21 @@ public class Jan3Problem2270NumberofWaystoSplitArray {
     public static void main(String[] args) {
         
     }
+    public static int waysToSplitArray(int[] nums) {
+        long leftSum = 0, rightSum = 0;
+      for (int num : nums) {
+          rightSum += num;
+      }
+
+      int validSplits = 0;
+      for (int i = 0; i < nums.length - 1; i++) {
+          leftSum += nums[i];
+          rightSum -= nums[i];
+          if (leftSum >= rightSum) {
+              validSplits++;
+          }
+      }
+
+      return validSplits; 
+  }
 }
