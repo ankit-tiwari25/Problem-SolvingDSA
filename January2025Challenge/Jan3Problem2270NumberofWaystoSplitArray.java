631cf2cd5bd253750e36e242ae1d2d1e4374becc
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-01-03 19:57:03 +0530

    2270.[Medium] Number of Ways to Split Array

---
+++ January2025Challenge/Jan3Problem2270NumberofWaystoSplitArray.java
@@ -0,0 +1,8 @@
+package January2025Challenge;
+
+/* 2270.[Medium] Number of Ways to Split Array */
+public class Jan3Problem2270NumberofWaystoSplitArray {
+    public static void main(String[] args) {
+        
+    }
+}
