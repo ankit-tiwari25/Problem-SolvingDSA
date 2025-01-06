c930b5fa0f580a0ab9291516c17f3e0454f18ed1
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-01-06 15:06:41 +0530

    1769. Minimum Number of Operations to Move All Balls to Each Box

---
+++ January2025Challenge/Jan6Problem1769MinimumNumberofOperationstoMoveAllBallstoEachBox.java
@@ -0,0 +1,7 @@
+package January2025Challenge;
+/* 
+ * 1769. [Medium] Minimum Number of Operations to Move All Balls to Each Box
+ */
+public class Jan6Problem1769MinimumNumberofOperationstoMoveAllBallstoEachBox {
+    
+}
