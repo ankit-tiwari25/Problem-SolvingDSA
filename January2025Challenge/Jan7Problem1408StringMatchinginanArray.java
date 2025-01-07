24fac4ce85ca93e360f4b6f625547eff264927a5
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-01-07 16:09:22 +0530

    1408. [Easy] String Matching in an Array

--- January2025Challenge/Jan7Problem1408StringMatchinginanArray.java
+++ January2025Challenge/Jan7Problem1408StringMatchinginanArray.java
@@ -1,9 +1,29 @@
 package January2025Challenge;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /*
  *  1408. [Easy] String Matching in an Array
  */
 public class Jan7Problem1408StringMatchinginanArray {
     public static void main(String[] args) {
-        
+       String[] words = {"mass","as","hero","superhero"};
+       System.out.println(stringMatching(words)); 
+    }
+    public static List<String> stringMatching(String[] words) {
+           int n = words.length;
+        List<String> ans = new ArrayList<>();
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (i != j && words[j].contains(words[i])) {
+                    ans.add(words[i]);
+                    break;
+                }
+            }
+        }
+
+        return ans;
     }
 }
