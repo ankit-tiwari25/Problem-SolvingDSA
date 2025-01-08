d81d034beb866abca233fd1632c76b2ecbefe250
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-01-08 14:46:45 +0530

    3042. [Easy] Count Prefix and Suffix Pairs I

---
+++ January2025Challenge/Jan8Problem3042CountPrefixandSuffixPairsI.java
@@ -0,0 +1,9 @@
+package January2025Challenge;
+
+/* 3042. [Easy] Count Prefix and Suffix Pairs I */
+public class Jan8Problem3042CountPrefixandSuffixPairsI {
+    public static void main(String[] args) {
+        
+    }
+
+}
