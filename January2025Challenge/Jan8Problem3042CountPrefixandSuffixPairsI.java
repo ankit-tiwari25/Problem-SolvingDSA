9ff905ea0c77dc3242bbace632807ff550472501
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-01-08 14:48:15 +0530

    [Easy] Count Prefix and Suffix Pairs I

--- January2025Challenge/Jan8Problem3042CountPrefixandSuffixPairsI.java
+++ January2025Challenge/Jan8Problem3042CountPrefixandSuffixPairsI.java
@@ -5,5 +5,16 @@ public class Jan8Problem3042CountPrefixandSuffixPairsI {
     public static void main(String[] args) {
         
     }
+    public static int countPrefixSuffixPairs(String[] words) {
+        int n = words.length, count = 0;
+      for (int i = 0; i < n; ++i) {
+          for (int j = i + 1; j < n; ++j) {
+              if (isPrefixAndSuffix(words[i], words[j])) {
+                  count++;
+              }
+          }
+      }
+      return count;
+  }
 
 }
