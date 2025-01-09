42049140ead6540f42d7fc704227f1d22adbdbc5
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-01-09 18:25:09 +0530

    2185.[Easy] Counting Words With a Given Prefix

--- January2025Challenge/Jan9Problem2185CountingWordsWithaGivenPrefix.java
+++ January2025Challenge/Jan9Problem2185CountingWordsWithaGivenPrefix.java
@@ -3,7 +3,19 @@ package January2025Challenge;
 /* 2185.[Easy] Counting Words With a Given Prefix */
 public class Jan9Problem2185CountingWordsWithaGivenPrefix {
     public static void main(String[] args) {
-        
+        String[] words = {"pay","attention","practice","attend"};
+        String pref = "at";
+        System.out.println(prefixCount(words, pref));
+
     }
-    
+    public static int prefixCount(String[] words, String pref) {
+        int count = 0;
+
+        for(String s : words){
+            if(s.startsWith(pref)){
+                count++;
+            }
+        }
+        return count;
+    }
 }
