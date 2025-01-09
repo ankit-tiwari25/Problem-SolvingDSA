601efb98b3afa2cea1e090e9254f336a5899162d
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-01-09 18:22:45 +0530

    2185. Counting Words With a Given Prefix

---
+++ January2025Challenge/Jan9Problem2185CountingWordsWithaGivenPrefix.java
@@ -0,0 +1,9 @@
+package January2025Challenge;
+
+/* 2185.[Easy] Counting Words With a Given Prefix */
+public class Jan9Problem2185CountingWordsWithaGivenPrefix {
+    public static void main(String[] args) {
+        
+    }
+    
+}
