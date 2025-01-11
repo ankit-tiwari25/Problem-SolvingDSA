dd1dd49a2d0116ea0c92bf732a48091231d16e02
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-01-11 23:46:49 +0530

    1400. [Medium] Construct K Palindrome Strings

--- January2025Challenge/Jan11Problem1400ConstructKPalindromeStrings.java
+++ January2025Challenge/Jan11Problem1400ConstructKPalindromeStrings.java
@@ -1,6 +1,28 @@
 /* 1400. [Medium] Construct K Palindrome Strings */
+
+import java.util.Arrays;
+
 public class Jan11Problem1400ConstructKPalindromeStrings {
     public static void main(String[] args) {
         
     }
+    public static boolean canConstruct(String s, int k) {
+        if (s.length() < k) return false;
+
+       char[] chars = s.toCharArray();
+       Arrays.sort(chars);
+       int oddCount = 0;
+
+       for (int i = 0; i < chars.length; ) {
+           char current = chars[i];
+           int count = 0;
+           while (i < chars.length && chars[i] == current) {
+               count++;
+               i++;
+           }
+           if (count % 2 != 0) oddCount++;
+       }
+
+       return oddCount <= k;
+   }
 }
