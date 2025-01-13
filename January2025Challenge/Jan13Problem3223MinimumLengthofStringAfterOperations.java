ed279b322a48b3dba34057b506930ccf78805901
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-01-13 18:31:00 +0530

    3223. [Medium] Minimum Length of String After Operations

--- January2025Challenge/Jan13Problem3223MinimumLengthofStringAfterOperations.java
+++ January2025Challenge/Jan13Problem3223MinimumLengthofStringAfterOperations.java
@@ -4,4 +4,21 @@ public class Jan13Problem3223MinimumLengthofStringAfterOperations {
     public static void main(String[] args) {
         
     }
+    public static int minimumLength(String s) {
+        int[] charFrequency = new int[26];
+       int totalLength = 0;
+       for (char c : s.toCharArray()) {
+           charFrequency[c - 'a']++;
+       }
+       for (int frequency : charFrequency) {
+           if (frequency == 0) continue;
+           if (frequency % 2 == 0) {
+               totalLength += 2;
+           } else {
+               totalLength += 1;
+           }
+       }
+       return totalLength;
+   }
+
 }
