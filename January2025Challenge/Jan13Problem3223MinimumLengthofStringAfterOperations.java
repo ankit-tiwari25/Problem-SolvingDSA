ca3299ce7bf1207bb1dcf2ce78da865be7963aad
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-01-13 18:27:01 +0530

    3223. [Medium] Minimum Length of String After Operations

---
+++ January2025Challenge/Jan13Problem3223MinimumLengthofStringAfterOperations.java
@@ -0,0 +1,7 @@
+package January2025Challenge;
+/* 3223. [Medium] Minimum Length of String After Operations */
+public class Jan13Problem3223MinimumLengthofStringAfterOperations {
+    public static void main(String[] args) {
+        
+    }
+}
