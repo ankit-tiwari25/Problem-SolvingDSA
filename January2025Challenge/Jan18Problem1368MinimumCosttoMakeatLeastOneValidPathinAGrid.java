358f6b57adc5c0971b7e1beca7cf592c3fdf9941
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-01-18 19:35:40 +0530

    1368. [Hard] Minimum Cost to Make at Least One Valid Path in a Grid

--- January2025Challenge/Jan18Problem1368MinimumCosttoMakeatLeastOneValidPathinAGrid.java
+++ January2025Challenge/Jan18Problem1368MinimumCosttoMakeatLeastOneValidPathinAGrid.java
@@ -7,7 +7,8 @@ import java.util.Deque;
 /* 1368. [Hard] Minimum Cost to Make at Least One Valid Path in a Grid */
 public class Jan18Problem1368MinimumCosttoMakeatLeastOneValidPathinAGrid {
     public static void main(String[] args) {
-        
+        int[][] grid = {{1,1,1,1},{2,2,2,2},{1,1,1,1},{2,2,2,2}};
+        System.out.println(minCost(grid));
     }
      public static int minCost(int[][] grid) {
          int m=grid.length;
