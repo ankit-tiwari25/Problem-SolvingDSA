ffb97c1c239e6a44d11c88fd7456390f567415fd
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-01-18 19:33:17 +0530

    1368. [Hard] Minimum Cost to Make at Least One Valid Path in a Grid

--- January2025Challenge/Jan18Problem1368MinimumCosttoMakeatLeastOneValidPathinAGrid.java
+++ January2025Challenge/Jan18Problem1368MinimumCosttoMakeatLeastOneValidPathinAGrid.java
@@ -1,5 +1,8 @@
+package January2025Challenge;
 
 /* 1368. [Hard] Minimum Cost to Make at Least One Valid Path in a Grid */
 public class Jan18Problem1368MinimumCosttoMakeatLeastOneValidPathinAGrid {
-    
+    public static void main(String[] args) {
+        
+    }
 }
