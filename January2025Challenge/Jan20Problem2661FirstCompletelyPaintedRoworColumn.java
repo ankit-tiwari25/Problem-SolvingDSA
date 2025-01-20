b1b2120a408a073d74aec10af8139115d72cd882
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-01-20 20:55:44 +0530

    2661. [Medium] First Completely Painted Row or Column

--- January2025Challenge/Jan20Problem2661FirstCompletelyPaintedRoworColumn.java
+++ January2025Challenge/Jan20Problem2661FirstCompletelyPaintedRoworColumn.java
@@ -7,7 +7,8 @@ import java.util.Map;
 /* 2661. [Medium] First Completely Painted Row or Column */
 public class Jan20Problem2661FirstCompletelyPaintedRoworColumn {
     public static void main(String[] args) {
-        
+        int[] arr = {1,3,4,2};
+        int[][] mat = {{1,4},{2,3}};
     }
     public static int firstCompleteIndex(int[] arr, int[][] mat) {
           int rows = mat.length, cols = mat[0].length;
