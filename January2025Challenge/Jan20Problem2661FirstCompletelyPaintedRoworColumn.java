625de9691927cff82028efd31a77288ba14fe6a6
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-01-20 20:52:37 +0530

    2661. [Medium] First Completely Painted Row or Column

--- January2025Challenge/Jan20Problem2661FirstCompletelyPaintedRoworColumn.java
+++ January2025Challenge/Jan20Problem2661FirstCompletelyPaintedRoworColumn.java
@@ -1,5 +1,7 @@
 package January2025Challenge;
 /* 2661. [Medium] First Completely Painted Row or Column */
 public class Jan20Problem2661FirstCompletelyPaintedRoworColumn {
-    
+    public static void main(String[] args) {
+        
+    }
 }
