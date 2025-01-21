022f81b33f16bd9d2dec8364408a53e180b9cc98
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-01-21 15:27:48 +0530

    2017.[Medium] Grid Game

--- January2025Challenge/Jan21Problem2017GridGame.java
+++ January2025Challenge/Jan21Problem2017GridGame.java
@@ -4,4 +4,20 @@ public class Jan21Problem2017GridGame {
     public static void main(String[] args) {
         
     }
+    public static long gridGame(int[][] grid) {
+        long minResult = Long.MAX_VALUE;
+        long row1Sum = 0;
+        for (int i = 0; i < grid[0].length; ++i) {
+            row1Sum += grid[0][i];
+        }
+        long row2Sum = 0;
+
+        for (int i = 0; i < grid[0].length; ++i) {
+            row1Sum -= grid[0][i];
+            minResult = Math.min(minResult, Math.max(row1Sum, row2Sum));
+            row2Sum += grid[1][i];
+        }
+
+        return minResult;   
+    }
 }
