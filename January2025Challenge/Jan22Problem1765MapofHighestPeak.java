0939e75a63d80a809d207dd05659d258d7206617
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-01-22 17:52:08 +0530

    1765.[Medium] Map of Highest Peak

--- January2025Challenge/Jan22Problem1765MapofHighestPeak.java
+++ January2025Challenge/Jan22Problem1765MapofHighestPeak.java
@@ -62,4 +62,12 @@ public class Jan22Problem1765MapofHighestPeak {
 
         return matrix;
     }
+    public static void display(int[][] mat){
+        for(int i = 0; i < mat.length; i++){
+            for(int j = 0; j < mat[i].length; j++){
+                System.out.print(" " + mat[i][j]);
+            }
+            System.out.println();
+        }
+    }
 }
