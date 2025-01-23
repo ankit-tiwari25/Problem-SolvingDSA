0ab3f93fb25dd06be5a391740ac290912edb0bcd
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-01-23 21:56:18 +0530

    1267. Count Servers that Communicate

--- January2025Challenge/Jan23Problem1267CountServersthatCommunicate.java
+++ January2025Challenge/Jan23Problem1267CountServersthatCommunicate.java
@@ -1,3 +1,5 @@
+package January2025Challenge;
+
 // 1267.[Medium] Count Servers that Communicate
 public class Jan23Problem1267CountServersthatCommunicate {
    public static void main(String[] args) {
