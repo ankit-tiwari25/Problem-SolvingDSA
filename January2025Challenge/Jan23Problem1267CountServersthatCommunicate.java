a715f5934cface24b8360277f4fcf4b3a664fea7
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-01-23 21:56:38 +0530

    1267. Count Servers that Communicate

--- January2025Challenge/Jan23Problem1267CountServersthatCommunicate.java
+++ January2025Challenge/Jan23Problem1267CountServersthatCommunicate.java
@@ -5,4 +5,23 @@ public class Jan23Problem1267CountServersthatCommunicate {
    public static void main(String[] args) {
     
    } 
+   public int countServers(int[][] grid) {
+    int[] Rows = new int[grid.length];
+   int[] Col = new int[grid[0].length];
+   for (int i = 0; i < grid.length; i++) {
+       for (int j = 0; j < grid[0].length; j++) {
+           Rows[i] += grid[i][j];
+           Col[j] += grid[i][j];
+       }
+   }
+   int ans = 0;
+   for (int i = 0; i < grid.length; i++) {
+       for (int j = 0; j < grid[0].length; j++) {
+           if (grid[i][j] == 1 && (Rows[i] > 1 || Col[j] > 1)) {
+               ans++;
+           }
+       }
+   }
+   return ans;
+}
 }
