21dd01544367410a770c8e428e319080a2b41e9c
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-01-25 16:30:39 +0530

    2948. Make Lexicographically Smallest Array by Swapping Elements

--- January2025Challenge/Jan25Problem2948MakeLexicographicallySmallestArraybySwappingElements.java
+++ January2025Challenge/Jan25Problem2948MakeLexicographicallySmallestArraybySwappingElements.java
@@ -1,5 +1,7 @@
 package January2025Challenge;
 /* 2948.[Medium] Make Lexicographically Smallest Array by Swapping Elements */
 public class Jan25Problem2948MakeLexicographicallySmallestArraybySwappingElements {
-    
+    public static void main(String[] args) {
+        
+    }
 }
