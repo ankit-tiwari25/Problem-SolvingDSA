976efbd8336c3ff93d7631f0d06c94112a3ccf4d
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-01-26 22:27:46 +0530

    2127. Maximum Employees to Be Invited to a Meeting

--- January2025Challenge/Jan26Problem2127MaximumEmployeestoBeInvitedtoaMeeting.java
+++ January2025Challenge/Jan26Problem2127MaximumEmployeestoBeInvitedtoaMeeting.java
@@ -1,3 +1,5 @@
+package January2025Challenge;
+
 /* 2127.[Hard] Maximum Employees to Be Invited to a Meeting */
 public class Jan26Problem2127MaximumEmployeestoBeInvitedtoaMeeting {
     public static void main(String[] args) {
