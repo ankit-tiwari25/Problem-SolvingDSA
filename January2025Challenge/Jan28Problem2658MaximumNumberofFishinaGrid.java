25f279b749b463e7916c40e6213494ca2eff86b4
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-01-28 19:05:35 +0530

    2658. Maximum Number of Fish in a Grid

--- January2025Challenge/Jan28Problem2658MaximumNumberofFishinaGrid.java
+++ January2025Challenge/Jan28Problem2658MaximumNumberofFishinaGrid.java
@@ -5,7 +5,15 @@ public class Jan28Problem2658MaximumNumberofFishinaGrid {
     public static void main(String[] args) {
         
     }
-    public int findMaxFish(int[][] grid) {
+    private static int dfs(int[][] grid, int i, int j) {
+        if (i<0 || i>=m || j<0 || j>=n || grid[i][j]==0) return 0;
+        int sum = grid[i][j];
+        grid[i][j] = 0;
+        for (int k=0; k<4; k++)
+            sum += dfs(grid, i+dir[k], j+dir[k+1]);
+        return sum;
+    }
+    public static int findMaxFish(int[][] grid) {
         m = grid.length; n = grid[0].length;
        int maxFish = 0;
        for (int i=0; i<m; i++)
