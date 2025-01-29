71c381d5938d1ee882501ea8839306697fb6610c
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-01-29 14:32:22 +0530

    684.[Medium] Redundant Connection

---
+++ January2025Challenge/Jan29Problem684RedundantConnection.java
@@ -0,0 +1,8 @@
+package January2025Challenge;
+
+/* 684.[Medium] Redundant Connection */
+public class Jan29Problem684RedundantConnection {
+    public static void main(String[] args) {
+        
+    }
+}
