afdccbbfcf50ad9a87a93fd4f17982329fa657cb
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-01-29 14:37:44 +0530

    684.[Medium] Redundant Connection

--- January2025Challenge/Jan29Problem684RedundantConnection.java
+++ January2025Challenge/Jan29Problem684RedundantConnection.java
@@ -53,4 +53,11 @@ private static void join(int u, int v) {
         }
     }
 }
+
+public static void display(int[] arr){
+    for(int i : arr){
+        System.out.print(" " + i);
+    }
+    System.out.println();
+}
 }
