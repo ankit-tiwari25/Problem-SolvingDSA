ba4c4bd00ad145363573b9995f2d284e5c4c66bb
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-01-30 14:53:57 +0530

    2493. Divide Nodes Into the Maximum Number of Groups

---
+++ January2025Challenge/Jan30Problem2493DivideNodesIntotheMaximumNumberofGroups.java
@@ -0,0 +1,6 @@
+/* 2493. Divide Nodes Into the Maximum Number of Groups */
+public class Jan30Problem2493DivideNodesIntotheMaximumNumberofGroups {
+    public static void main(String[] args) {
+        
+    }
+}
