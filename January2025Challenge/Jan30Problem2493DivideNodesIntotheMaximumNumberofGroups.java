0c733927565ae46336f1673b47049005a890e05a
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-01-30 14:54:50 +0530

    2493. Divide Nodes Into the Maximum Number of Groups

--- January2025Challenge/Jan30Problem2493DivideNodesIntotheMaximumNumberofGroups.java
+++ January2025Challenge/Jan30Problem2493DivideNodesIntotheMaximumNumberofGroups.java
@@ -1,5 +1,12 @@
+package January2025Challenge;
+
+import java.util.List;
+
 /* 2493. Divide Nodes Into the Maximum Number of Groups */
 public class Jan30Problem2493DivideNodesIntotheMaximumNumberofGroups {
+    private static int[] color; // Tracks bipartition colors (0/1)
+    private static List<List<Integer>> adj; // Adjacency list
+    private static int n; // Number of nodes
     public static void main(String[] args) {
         
     }
