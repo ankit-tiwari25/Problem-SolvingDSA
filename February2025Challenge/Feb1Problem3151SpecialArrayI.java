748e51cb991cc4947cfadc352b186762b92b335a
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-02-01 12:02:55 +0530

    3151.[Easy] Special Array I

--- February2025Challenge/Feb1Problem3151SpecialArrayI.java
+++ February2025Challenge/Feb1Problem3151SpecialArrayI.java
@@ -4,4 +4,12 @@ public class Feb1Problem3151SpecialArrayI {
     public static void main(String[] args) {
         
     }
+    public static boolean isArraySpecial(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+         if (nums[i] % 2 == nums[i + 1] % 2) {
+             return false;
+         }
+     }
+     return true;
+ }
 }
