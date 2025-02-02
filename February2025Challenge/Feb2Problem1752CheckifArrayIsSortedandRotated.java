b9ec9e24b85ccd42a5b3ab61619e287f5ef49e28
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-02-02 12:36:08 +0530

    1752. Check if Array Is Sorted and Rotated

--- February2025Challenge/Feb2Problem1752CheckifArrayIsSortedandRotated.java
+++ February2025Challenge/Feb2Problem1752CheckifArrayIsSortedandRotated.java
@@ -5,4 +5,16 @@ public class Feb2Problem1752CheckifArrayIsSortedandRotated {
   public static void main(String[] args) {
     
   }  
+  public static boolean check(int[] nums) {
+    int count=0;
+  for(int i=0;i<nums.length-1;i++){
+      if(nums[i]>nums[i+1]){
+          count++;
+      }
+  }
+  if(nums[0]<nums[nums.length-1]){
+      count++;
+  }
+  return count<=1;
+}
 }
