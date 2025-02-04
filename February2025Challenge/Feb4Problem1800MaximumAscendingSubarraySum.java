cbe5183a715e4323780898166c6301bf7522d200
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-02-04 13:11:00 +0530

    1800. [Easy] Maximum Ascending Subarray Sum

--- February2025Challenge/Feb4Problem1800MaximumAscendingSubarraySum.java
+++ February2025Challenge/Feb4Problem1800MaximumAscendingSubarraySum.java
@@ -4,7 +4,7 @@ public class Feb4Problem1800MaximumAscendingSubarraySum {
     public static void main(String[] args) {
         
     }
-    public int maxAscendingSum(int[] nums) {
+    public static int maxAscendingSum(int[] nums) {
           int curr = nums[0], ans = nums[0];
         for (int i = 1; i < nums.length; i++) {
             curr = nums[i] > nums[i - 1] ? curr + nums[i] : nums[i];
