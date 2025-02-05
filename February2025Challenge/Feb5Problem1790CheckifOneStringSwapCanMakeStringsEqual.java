f793431c8fa162f6db3d5ae2c74d9c83ac12f722
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-02-05 15:10:18 +0530

    1790. [Easy] Check if One String Swap Can Make Strings Equal

---
+++ February2025Challenge/Feb5Problem1790CheckifOneStringSwapCanMakeStringsEqual.java
@@ -0,0 +1,7 @@
+package February2025Challenge;
+/* 1790. [Easy] Check if One String Swap Can Make Strings Equal */
+public class Feb5Problem1790CheckifOneStringSwapCanMakeStringsEqual {
+    public static void main(String[] args) {
+        
+    }
+}
