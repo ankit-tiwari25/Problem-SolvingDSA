2361d957f2df9815e0ce6b0fa3da3523aae39a3b
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-02-06 11:44:16 +0530

    1726.[Medium] Tuple with Same Product

---
+++ February2025Challenge/Feb6Problem1726TuplewithSameProduct.java
@@ -0,0 +1,8 @@
+package February2025Challenge;
+
+/* 1726.[Medium] Tuple with Same Product */
+public class Feb6Problem1726TuplewithSameProduct {
+   public static void main(String[] args) {
+    
+   } 
+}
