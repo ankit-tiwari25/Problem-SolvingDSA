1889b6ac7712ef59faee9796f20dee23ebb1e62d
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-02-07 14:43:32 +0530

    3160. Find the Number of Distinct Colors Among the Balls

--- February2025Challenge/Feb7Problem3160FindtheNumberofDistinctColorsAmongtheBalls.java
+++ February2025Challenge/Feb7Problem3160FindtheNumberofDistinctColorsAmongtheBalls.java
@@ -1,7 +1,36 @@
 package February2025Challenge;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /* 3160.[Medium] Find the Number of Distinct Colors Among the Balls */
 public class Feb7Problem3160FindtheNumberofDistinctColorsAmongtheBalls {
     public static void main(String[] args) {
         
+    }
+     public static int[] queryResults(int limit, int[][] queries) {
+         Map<Integer, Integer> bal=new HashMap<>();
+        Map<Integer, Integer> col=new HashMap<>();
+        int n=queries.length;
+        int[] res=new int[n];
+
+        for(int i=0; i<n; i++){
+            if(!bal.containsKey(queries[i][0])){
+                bal.put(queries[i][0],queries[i][1]);
+            }else{
+                if(col.get(bal.get(queries[i][0]))==1){
+                    col.remove(bal.get(queries[i][0]));
+                }else{
+                    col.put(bal.get(queries[i][0]), col.get(bal.get(queries[i][0]))-1);
+                }
+                bal.put(queries[i][0],queries[i][1]);
+            }
+
+            col.put(queries[i][1],col.getOrDefault(queries[i][1],0)+1);
+
+            res[i]=col.size();
+        }
+        
+        return res;
     }
 }
