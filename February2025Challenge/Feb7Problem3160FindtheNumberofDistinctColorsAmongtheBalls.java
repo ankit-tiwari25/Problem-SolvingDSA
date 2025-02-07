bdf3322b3e7b1fa1f035a68de61f09b79889e7d3
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-02-07 14:42:53 +0530

    3160. Find the Number of Distinct Colors Among the Balls

---
+++ February2025Challenge/Feb7Problem3160FindtheNumberofDistinctColorsAmongtheBalls.java
@@ -0,0 +1,7 @@
+package February2025Challenge;
+/* 3160.[Medium] Find the Number of Distinct Colors Among the Balls */
+public class Feb7Problem3160FindtheNumberofDistinctColorsAmongtheBalls {
+    public static void main(String[] args) {
+        
+    }
+}
