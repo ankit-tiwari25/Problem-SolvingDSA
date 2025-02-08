8291011c4422ee0b5bcc23df7547d86e94f9287f
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-02-08 23:54:02 +0530

    2349. Design a Number Container System

--- February2025Challenge/Feb8Problem2349DesignaNumberContainerSystem.java
+++ February2025Challenge/Feb8Problem2349DesignaNumberContainerSystem.java
@@ -1,5 +1,12 @@
 /* 2349.[Medium] Design a Number Container System */
+
+import java.util.Map;
+import java.util.PriorityQueue;
+
 public class Feb8Problem2349DesignaNumberContainerSystem {
+      private Map<Integer, PriorityQueue<Integer>> res;
+    private Map<Integer, Integer> index_val;
+
     public static void main(String[] args) {
         
     }
