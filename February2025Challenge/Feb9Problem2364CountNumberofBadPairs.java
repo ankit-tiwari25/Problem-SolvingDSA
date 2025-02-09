705f2a6f32e925380f2a3de45befe38580f73f57
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-02-09 17:43:36 +0530

    2364.[Medium] Count Number of Bad Pairs

--- February2025Challenge/Feb9Problem2364CountNumberofBadPairs.java
+++ February2025Challenge/Feb9Problem2364CountNumberofBadPairs.java
@@ -1,8 +1,20 @@
 package February2025Challenge;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /* 2364.[Medium] Count Number of Bad Pairs  */
 public class Feb9Problem2364CountNumberofBadPairs {
     public static void main(String[] args) {
         
+    }
+    public static long countBadPairs(int[] nums) {
+        Map<Integer, Integer> bag = new HashMap<>();
+        long count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            count += -1 + bag.merge(nums[i] - i, 1, Integer::sum);
+        }
+        int len = nums.length;
+        return 1L * len * (len - 1) / 2 - count; 
     }
 }
