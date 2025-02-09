1d6a192dc542c1ba6d6512166210ee471af17451
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-02-09 17:42:47 +0530

    2364.[Medium] Count Number of Bad Pairs

---
+++ February2025Challenge/Feb9Problem2364CountNumberofBadPairs.java
@@ -0,0 +1,8 @@
+package February2025Challenge;
+
+/* 2364.[Medium] Count Number of Bad Pairs  */
+public class Feb9Problem2364CountNumberofBadPairs {
+    public static void main(String[] args) {
+        
+    }
+}
