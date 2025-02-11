c24d614b514ade059db9ddfd54c397ccafd87b43
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-02-11 23:32:55 +0530

    1910. Remove All Occurrences of a Substring

--- February2025Challenge/Feb11Problem1910RemoveAllOccurrencesofaSubstring.java
+++ February2025Challenge/Feb11Problem1910RemoveAllOccurrencesofaSubstring.java
@@ -1,3 +1,5 @@
+package February2025Challenge;
+
 // 1910. [Medium] Remove All Occurrences of a Substring
 public class Feb11Problem1910RemoveAllOccurrencesofaSubstring {
     public static void main(String[] args) {
