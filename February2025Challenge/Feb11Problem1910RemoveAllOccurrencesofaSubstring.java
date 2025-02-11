f0bef05c4d6298c388cd180db352fdd893cd82f0
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-02-11 23:32:40 +0530

    1910. Remove All Occurrences of a Substring

--- February2025Challenge/Feb11Problem1910RemoveAllOccurrencesofaSubstring.java
+++ February2025Challenge/Feb11Problem1910RemoveAllOccurrencesofaSubstring.java
@@ -3,4 +3,29 @@ public class Feb11Problem1910RemoveAllOccurrencesofaSubstring {
     public static void main(String[] args) {
         
     }
+    public static String removeOccurrences(String s, String part) {
+        char[] input = s.toCharArray();
+ char[] target = part.toCharArray();
+ char[] resultStack = new char[input.length];
+ int stackSize = 0, targetLength = target.length;
+ char targetEndChar = target[targetLength - 1];
+
+ for (char currentChar : input) {
+     resultStack[stackSize++] = currentChar;
+
+     if (currentChar == targetEndChar && stackSize >= targetLength) {
+         int i = stackSize - 1, j = targetLength - 1;
+         
+         while (j >= 0 && resultStack[i] == target[j]) {
+             i--;
+             j--;
+         }
+
+         if (j < 0) {
+             stackSize = i + 1;
+         }
+     }
+ }
+ return new String(resultStack, 0, stackSize); 
+}
 }
