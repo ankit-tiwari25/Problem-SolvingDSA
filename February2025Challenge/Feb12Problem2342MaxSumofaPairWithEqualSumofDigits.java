0c376ac5cdaabbedb732198eef95403fff6a96a0
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-02-12 16:50:59 +0530

    2342. Max Sum of a Pair With Equal Sum of Digits

--- February2025Challenge/Feb12Problem2342MaxSumofaPairWithEqualSumofDigits.java
+++ February2025Challenge/Feb12Problem2342MaxSumofaPairWithEqualSumofDigits.java
@@ -1,3 +1,7 @@
+
+/* 2342. [Medium] Max Sum of a Pair With Equal Sum of Digits */
 public class Feb12Problem2342MaxSumofaPairWithEqualSumofDigits {
-    
+    public static void main(String[] args) {
+        
+    }
 }
