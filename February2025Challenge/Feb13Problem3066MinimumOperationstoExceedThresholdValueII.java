0552d526ad1ae7204c8bb51aea5d4dc4bcbbac87
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-02-13 19:44:33 +0530

    3066. Minimum Operations to Exceed Threshold Value II

--- February2025Challenge/Feb13Problem3066MinimumOperationstoExceedThresholdValueII.java
+++ February2025Challenge/Feb13Problem3066MinimumOperationstoExceedThresholdValueII.java
@@ -1,6 +1,31 @@
 /* 3066.[Medium] Minimum Operations to Exceed Threshold Value II */
+
+import java.util.PriorityQueue;
+
 public class Feb13Problem3066MinimumOperationstoExceedThresholdValueII {
     public static void main(String[] args) {
         
+    }
+     public int minOperations(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int num : nums) {
+            if (num < k) pq.add(num);
+        }
+
+        int operations = 0;
+
+        while (!pq.isEmpty()) {
+            int x = pq.poll();
+            operations++;
+
+            if (pq.isEmpty()) break;
+
+            int y = pq.poll();
+            long newValue = 2L * x + y;
+
+            if (newValue < k) pq.add((int) newValue);
+        }
+
+        return operations;
     }
 }
