cf5a09e4518c350ec3100896d8700e200511a773
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-02-15 17:27:34 +0530

    2698. Find the Punishment Number of an Integer

--- February2025Challenge/Feb15Problem2698FindthePunishmentNumberofanInteger.java
+++ February2025Challenge/Feb15Problem2698FindthePunishmentNumberofanInteger.java
@@ -2,6 +2,7 @@
 public class Feb15Problem2698FindthePunishmentNumberofanInteger {
     public static void main(String[] args) {
         int n = 10;
+        System.out.println(punishmentNumber(n));
     }
     public static int punishmentNumber(int n) {
         int[] arr = {1,9,10,36,45,55,82,91,99,100,235,297,369,370,379,414,657,675,703,756,792,909,918,945,964,990,991,999,1000};
