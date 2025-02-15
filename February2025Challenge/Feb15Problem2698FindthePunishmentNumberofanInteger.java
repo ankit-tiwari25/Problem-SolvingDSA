00637a1436b14106fc74394a005cb5a50e5002e5
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-02-15 17:25:55 +0530

    2698. Find the Punishment Number of an Integer

--- February2025Challenge/Feb15Problem2698FindthePunishmentNumberofanInteger.java
+++ February2025Challenge/Feb15Problem2698FindthePunishmentNumberofanInteger.java
@@ -1,3 +1,4 @@
+/* 2698.[Medium] Find the Punishment Number of an Integer */
 public class Feb15Problem2698FindthePunishmentNumberofanInteger {
     public static void main(String[] args) {
         
