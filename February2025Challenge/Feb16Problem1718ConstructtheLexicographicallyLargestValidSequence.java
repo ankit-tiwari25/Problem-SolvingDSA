338d9e567cf4878d94b94981fbd8927ba801a879
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-02-16 15:10:33 +0530

    1718. Construct the Lexicographically Largest Valid Sequence

--- February2025Challenge/Feb16Problem1718ConstructtheLexicographicallyLargestValidSequence.java
+++ February2025Challenge/Feb16Problem1718ConstructtheLexicographicallyLargestValidSequence.java
@@ -9,4 +9,35 @@ public class Feb16Problem1718ConstructtheLexicographicallyLargestValidSequence {
         backtrack(result, used, n, 0);
         return result;  
     }
+    private static boolean backtrack(int[] result, boolean[] used, int n, int index){
+        while(index < result.length && result[index] != 0){
+            index++;
+        }
+        if(index == result.length){
+            return true;
+        }
+        for(int i = n; i >= 1; i--){
+            if(used[i]) continue;
+
+            if(i == 1){
+                result[index] = 1;
+                used[1] = true;
+                if(backtrack(result, used, n, index + 1)) return true;
+                result[index] = 0;
+                used[1] = false;
+            }
+            else{
+                if(index + i < result.length && result[index + i] == 0){
+                    result[index] = i;
+                    result[index + i] = i;
+                    used[i] = true;
+                    if(backtrack(result, used, n, index + 1)) return true;
+                    result[index] = 0;
+                    result[index + i] = 0;
+                    used[i] = false;
+                }
+            }
+        }
+        return false;
+    }
 }
