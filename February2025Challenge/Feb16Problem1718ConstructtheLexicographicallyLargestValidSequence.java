d0a743a35ab6e76bcefd5ff3856ad05dd6c190fd
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-02-16 15:11:11 +0530

    1718. Construct the Lexicographically Largest Valid Sequence

--- February2025Challenge/Feb16Problem1718ConstructtheLexicographicallyLargestValidSequence.java
+++ February2025Challenge/Feb16Problem1718ConstructtheLexicographicallyLargestValidSequence.java
@@ -1,7 +1,8 @@
 /* 1718.[Medium] Construct the Lexicographically Largest Valid Sequence */
 public class Feb16Problem1718ConstructtheLexicographicallyLargestValidSequence {
     public static void main(String[] args) {
-        
+        int n = 3;
+        display(constructDistancedSequence(n));
     }
     public static int[] constructDistancedSequence(int n) {
         int[] result = new int[2 * n - 1];
