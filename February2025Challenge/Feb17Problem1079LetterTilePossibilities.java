da5e1170b5b0a325f0af25edf3f042ebc0ee0e75
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-02-17 11:01:07 +0530

    1079. Letter Tile Possibilities

--- February2025Challenge/Feb17Problem1079LetterTilePossibilities.java
+++ February2025Challenge/Feb17Problem1079LetterTilePossibilities.java
@@ -1,7 +1,7 @@
 /*1079.[Medium] Letter Tile Possibilities  */
 public class Feb17Problem1079LetterTilePossibilities {
     public static void main(String[] args) {
-        
+        String tiles = "AAB";
     }
     public static int numTilePossibilities(String tiles) {
         int[] counts = new int[26];
