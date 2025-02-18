f7a8abf49e960b5c0d800bbc6d4326b61a07dfa4
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-02-18 21:02:59 +0530

    2375. Construct Smallest Number From DI String

--- February2025Challenge/Feb18Problem2375ConstructSmallestNumberFromDIString.java
+++ February2025Challenge/Feb18Problem2375ConstructSmallestNumberFromDIString.java
@@ -4,4 +4,22 @@ public class Feb18Problem2375ConstructSmallestNumberFromDIString {
     public static void main(String[] args) {
         
     }
+    public static String smallestNumber(String pattern) {
+        int n = pattern.length();
+       StringBuilder result = new StringBuilder();
+       int[] stack = new int[n + 1]; 
+       int index = 0;
+
+       for (int i = 0; i <= n; i++) {
+           stack[index++] = i + 1;
+
+           if (i == n || pattern.charAt(i) == 'I') {
+               while (index > 0) {
+                   result.append(stack[--index]);
+               }
+           }
+       }
+
+       return result.toString();
+   }
 }
