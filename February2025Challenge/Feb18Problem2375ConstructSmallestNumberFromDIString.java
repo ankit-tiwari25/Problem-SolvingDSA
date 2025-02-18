7a0165fb17f8569f876b2ca6f12a2bc5782de1fe
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-02-18 21:02:33 +0530

    2375. Construct Smallest Number From DI String

--- February2025Challenge/Feb18Problem2375ConstructSmallestNumberFromDIString.java
+++ February2025Challenge/Feb18Problem2375ConstructSmallestNumberFromDIString.java
@@ -1,5 +1,7 @@
 
 /*2375. Construct Smallest Number From DI String */
 public class Feb18Problem2375ConstructSmallestNumberFromDIString {
-    
+    public static void main(String[] args) {
+        
+    }
 }
