d49f2f0a88879799a54108a3d91f2f7e33a9d0b5
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-02-19 16:18:01 +0530

    1415. The k-th Lexicographical String of All Happy Strings of Length n

--- February2025Challenge/Feb19Problem1415ThekthLexicographicalStringofAllHappyStringsofLengthn.java
+++ February2025Challenge/Feb19Problem1415ThekthLexicographicalStringofAllHappyStringsofLengthn.java
@@ -3,4 +3,18 @@ public class Feb19Problem1415ThekthLexicographicalStringofAllHappyStringsofLengt
     public static void main(String[] args) {
         
     }
+    public String dfs(StringBuilder prefix, int n, int k){
+        if (n == 0)
+            return prefix.toString();
+        for (char c = 'a'; c <= 'c'; c++) {
+            if (prefix.length() > 0 && c == prefix.charAt(prefix.length() - 1))
+                continue;
+            int cnt = (int) Math.pow(2, n2 - prefix.length() - 1);
+            if (cnt >= k)
+                return dfs(prefix.append(c), n - 1, k);
+            else
+                k -= cnt;
+        }
+        return "";
+    }
 }
