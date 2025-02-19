476f17b14e576b66b66055c56b95fae3501de532
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-02-19 16:19:02 +0530

    1415. The k-th Lexicographical String of All Happy Strings of Length n

--- February2025Challenge/Feb19Problem1415ThekthLexicographicalStringofAllHappyStringsofLengthn.java
+++ February2025Challenge/Feb19Problem1415ThekthLexicographicalStringofAllHappyStringsofLengthn.java
@@ -1,5 +1,6 @@
 /*1415. [Medium] The k-th Lexicographical String of All Happy Strings of Length n */
 public class Feb19Problem1415ThekthLexicographicalStringofAllHappyStringsofLengthn {
+    static int n2;
     public static void main(String[] args) {
         
     }
