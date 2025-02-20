6a4466301a0fc765b443427c3a5530664ce38d82
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-02-20 11:27:48 +0530

    1980. Find Unique Binary String

--- February2025Challenge/Feb20Problem1980FindUniqueBinaryString.java
+++ February2025Challenge/Feb20Problem1980FindUniqueBinaryString.java
@@ -1,7 +1,7 @@
 /* 1980.[Medium] Find Unique Binary String */
 public class Feb20Problem1980FindUniqueBinaryString {
     public static void main(String[] args) {
-        
+        String[] nums = {"01","10"};
     }
     public static String findDifferentBinaryString(String[] nums) {
         StringBuilder result = new StringBuilder();
