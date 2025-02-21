0fe0a24ab724619f13724be21545c69e06cb5002
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-02-21 13:19:31 +0530

    1261. Find Elements in a Contaminated Binary Tree

--- February2025Challenge/Feb21Problem1261FindElementsinaContaminatedBinaryTree.java
+++ February2025Challenge/Feb21Problem1261FindElementsinaContaminatedBinaryTree.java
@@ -1,6 +1,9 @@
 public class Feb21Problem1261FindElementsinaContaminatedBinaryTree {
     TreeNode groot;
-    public FindElements(TreeNode root) {
+    public static void main(String[] args) {
+        
+    }
+    public TreeNode FindElements(TreeNode root) {
          if(root!=null && root.val==-1){
             root.val=0;                
         }
