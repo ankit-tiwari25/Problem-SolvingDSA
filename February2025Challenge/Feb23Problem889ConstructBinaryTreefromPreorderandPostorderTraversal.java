7d2f0477718a186243ad1294a438e28bcb17a89c
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-02-23 13:14:34 +0530

    889. Construct Binary Tree from Preorder and Postorder Traversal

--- February2025Challenge/Feb23Problem889ConstructBinaryTreefromPreorderandPostorderTraversal.java
+++ February2025Challenge/Feb23Problem889ConstructBinaryTreefromPreorderandPostorderTraversal.java
@@ -1,3 +1,6 @@
 public class Feb23Problem889ConstructBinaryTreefromPreorderandPostorderTraversal {
-    
+    public TreeNode constructFromPrePost(int[] preorder, int[] postorder) {
+        int[] index = new int[]{0};
+         return construct(preorder, postorder, index, 0, preorder.length - 1);  
+     }
 }
