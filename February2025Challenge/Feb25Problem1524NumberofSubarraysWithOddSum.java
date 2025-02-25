1cc0c2da653d399ff2704d19beaf83e6cb590486
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-02-25 06:04:55 +0530

    1524. Number of Sub-arrays With Odd Sum

--- February2025Challenge/Feb25Problem1524NumberofSubarraysWithOddSum.java
+++ February2025Challenge/Feb25Problem1524NumberofSubarraysWithOddSum.java
@@ -1,4 +1,6 @@
 /* 1524.[Medium] Number of Sub-arrays With Odd Sum */
 public class Feb25Problem1524NumberofSubarraysWithOddSum {
-    
+    public static void main(String[] args) {
+        
+    }
 }
