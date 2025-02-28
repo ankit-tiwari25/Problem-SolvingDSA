b71944953bf7a04e33b450ff0e09b8a648d35e49
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-02-28 12:32:11 +0530

    1092. Shortest Common Supersequence

--- February2025Challenge/Feb28Problem1092ShortestCommonSupersequence.java
+++ February2025Challenge/Feb28Problem1092ShortestCommonSupersequence.java
@@ -1,3 +1,18 @@
+import java.util.concurrent.Callable;
+import java.util.concurrent.FutureTask;
+
 public class Feb28Problem1092ShortestCommonSupersequence {
-    
+     public String shortestCommonSupersequence(String str1, String str2) {
+         String res = "";
+        try{
+            Callable<String> task = () -> generateLCS(str1, str2);
+            FutureTask<String> future = new FutureTask<>(task);
+            new Thread(future).start();
+            res = future.get();
+        }
+        catch(Exception e){
+
+        }
+        return res;
+    }
 }
