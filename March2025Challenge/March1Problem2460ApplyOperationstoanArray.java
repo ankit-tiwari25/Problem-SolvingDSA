3b1f9783f923cc7ee78382103c61a85afdd3d12e
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-03-01 13:57:45 +0530

    2460. Apply Operations to an Array

--- March2025Challenge/March1Problem2460ApplyOperationstoanArray.java
+++ March2025Challenge/March1Problem2460ApplyOperationstoanArray.java
@@ -1,5 +1,25 @@
 package March2025Challenge;
 /* 2460.[Easy] Apply Operations to an Array */
 public class March1Problem2460ApplyOperationstoanArray {
-    
+    public static int[] applyOperations(int[] nums) {
+        int[] result = new int[nums.length];
+        int count = 0;
+        int i;
+        for(i = 0; i < nums.length - 1; i++){
+            if(nums[i] != 0){
+                if(nums[i] == nums[i + 1]){
+                    result[count] = nums[i] * 2;
+                    i++;
+                }
+                else{
+                    result[count] = nums[i];
+                }
+                count++;
+            }
+        }
+        if(i != nums.length){
+            result[count] = nums[nums.length - 1];
+        }
+        return result; 
+    } 
 }
