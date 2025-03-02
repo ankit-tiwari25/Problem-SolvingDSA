20f0c727fe1ce7c4e24f358914412010d3c9d300
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-03-02 19:56:17 +0530

    2570.[Easy] Merge Two 2D Arrays by Summing Values

--- March2025Challenge/March2Problem2570MergeTwo2DArraysbySummingValues.java
+++ March2025Challenge/March2Problem2570MergeTwo2DArraysbySummingValues.java
@@ -9,6 +9,7 @@ public class March2Problem2570MergeTwo2DArraysbySummingValues {
         int[][]  nums1 = {{1,2},{2,3},{4,5}};
         int[][]  nums2 = {{1,4},{3,2},{4,1}};
         int[][] ans = mergeArrays(nums1, nums2);
+        display(ans);
     }
      public static int[][] mergeArrays(int[][] nums1, int[][] nums2) {
          // Initialize pointers for both arrays
@@ -59,6 +60,11 @@ public class March2Problem2570MergeTwo2DArraysbySummingValues {
     }
 
     public static void display(int[][] arr){
-        
+        for(int i = 0; i < arr.length; i++){
+            for(int j = 0; j < arr[i].length; j++){
+                System.out.print(" " + arr[i][j]);
+            }
+            System.out.println();
+        }
     }
 }
