552980e365f626a12dc02384eab48b9f8bc2f837
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-03-02 19:38:22 +0530

    2460. Apply Operations to an Array

--- March2025Challenge/March2Problem2570MergeTwo2DArraysbySummingValues.java
+++ March2025Challenge/March2Problem2570MergeTwo2DArraysbySummingValues.java
@@ -1,5 +1,7 @@
 package March2025Challenge;
 /* 2570.[Easy] Merge Two 2D Arrays by Summing Values */
 public class March2Problem2570MergeTwo2DArraysbySummingValues {
-    
+    public static void main(String[] args) {
+        
+    }
 }
