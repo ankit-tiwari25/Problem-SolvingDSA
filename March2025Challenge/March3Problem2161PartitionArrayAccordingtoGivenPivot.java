686d2ee81aeba1a55d0e4a5d6e8487ccd75692fe
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-03-03 18:35:55 +0530

    2161. Partition Array According to Given Pivot

--- March2025Challenge/March3Problem2161PartitionArrayAccordingtoGivenPivot.java
+++ March2025Challenge/March3Problem2161PartitionArrayAccordingtoGivenPivot.java
@@ -1,5 +1,7 @@
 package March2025Challenge;
 /* 2161. [Medium] Partition Array According to Given Pivot */
 public class March3Problem2161PartitionArrayAccordingtoGivenPivot {
-    
+    public static void main(String[] args) {
+        
+    }
 }
