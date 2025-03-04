9672cc47824450c148cd357e86ea17acc4574fe0
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-03-04 20:03:56 +0530

    1780. Check if Number is a Sum of Powers of Three

--- March2025Challenge/March4Problem1780CheckifNumberisaSumofPowersofThree.java
+++ March2025Challenge/March4Problem1780CheckifNumberisaSumofPowersofThree.java
@@ -1,5 +1,5 @@
 package March2025Challenge;
-/* 1780. Check if Number is a Sum of Powers of Three */
+/* 1780. [Medium] Check if Number is a Sum of Powers of Three */
 public class March4Problem1780CheckifNumberisaSumofPowersofThree {
     
 }
