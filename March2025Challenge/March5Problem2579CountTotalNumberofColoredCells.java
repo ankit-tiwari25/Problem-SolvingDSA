3863e9b078095583edb9e96d02bfa9e71cc0cc42
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-03-05 19:58:30 +0530

    2579. Count Total Number of Colored Cells

--- March2025Challenge/March5Problem2579CountTotalNumberofColoredCells.java
+++ March2025Challenge/March5Problem2579CountTotalNumberofColoredCells.java
@@ -1,4 +1,6 @@
 /* 2579.[Medium] Count Total Number of Colored Cells */
 public class March5Problem2579CountTotalNumberofColoredCells {
-    
+    public static void main(String[] args) {
+        
+    }
 }
