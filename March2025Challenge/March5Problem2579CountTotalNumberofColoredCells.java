b43020e95057d02a628d8a894c0e95bfd04e3b59
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-03-05 20:00:36 +0530

    2579. Count Total Number of Colored Cells

--- March2025Challenge/March5Problem2579CountTotalNumberofColoredCells.java
+++ March2025Challenge/March5Problem2579CountTotalNumberofColoredCells.java
@@ -3,10 +3,10 @@ package March2025Challenge;
 /* 2579.[Medium] Count Total Number of Colored Cells */
 public class March5Problem2579CountTotalNumberofColoredCells {
     public static void main(String[] args) {
-        
+        int n = 1;
+        System.out.println(coloredCells(n));
     }
     public static long coloredCells(int n) {
         return 1 + 4L * n * (n - 1) / 2;
-
-}
+    }
 }
