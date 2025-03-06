1f1f0999283c62b81c1244b8c882d82e6d7f8b73
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-03-06 13:29:13 +0530

    2965. Find Missing and Repeated Values

--- March2025Challenge/March6Problem2965FindMissingandRepeatedValues.java
+++ March2025Challenge/March6Problem2965FindMissingandRepeatedValues.java
@@ -5,4 +5,23 @@ public class March6Problem2965FindMissingandRepeatedValues {
     public static void main(String[] args) {
         
     }
+    public static int[] findMissingAndRepeatedValues(int[][] grid) {
+        int n = grid.length;
+       int size = n * n;
+       int[] freq = new int[size + 1];
+       int repeated = -1, missing = -1;
+
+       for (int[] row : grid) {
+           for (int num : row) {
+               freq[num]++;
+           }
+       }
+
+       for (int num = 1; num <= size; num++) {
+           if (freq[num] == 2) repeated = num;
+           if (freq[num] == 0) missing = num;
+       }
+
+       return new int[]{repeated, missing};
+   }
 }
