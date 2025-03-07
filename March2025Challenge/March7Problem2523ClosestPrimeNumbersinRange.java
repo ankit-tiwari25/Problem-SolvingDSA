822c74ee8f421fc4c291426809bcefbf284cec46
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-03-07 16:45:29 +0530

    2523. Closest Prime Numbers in Range

--- March2025Challenge/March7Problem2523ClosestPrimeNumbersinRange.java
+++ March2025Challenge/March7Problem2523ClosestPrimeNumbersinRange.java
@@ -1,5 +1,7 @@
 package March2025Challenge;
 /* 2523.[Medium] Closest Prime Numbers in Range */
 public class March7Problem2523ClosestPrimeNumbersinRange {
-    
+    public static void main(String[] args) {
+        
+    }
 }
