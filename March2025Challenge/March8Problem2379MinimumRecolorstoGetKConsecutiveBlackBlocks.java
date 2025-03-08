5479e503956814e1d97dfeefdbf238adcc05c93c
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-03-08 19:10:48 +0530

    2379. Minimum Recolors to Get K Consecutive Black Blocks

--- March2025Challenge/March8Problem2379MinimumRecolorstoGetKConsecutiveBlackBlocks.java
+++ March2025Challenge/March8Problem2379MinimumRecolorstoGetKConsecutiveBlackBlocks.java
@@ -3,4 +3,15 @@ public class March8Problem2379MinimumRecolorstoGetKConsecutiveBlackBlocks {
     public static void main(String[] args) {
         
     }
+    public int minimumRecolors(String blocks, int k) {
+        int blackCount = 0, ans = Integer.MAX_VALUE;
+      
+          for (int i = 0; i < blocks.length(); i++) {
+              if (i - k >= 0 && blocks.charAt(i - k) == 'B') blackCount--;
+              if (blocks.charAt(i) == 'B') blackCount++;
+              ans = Math.min(ans, k - blackCount);
+          }
+          
+          return ans;  
+      }
 }
