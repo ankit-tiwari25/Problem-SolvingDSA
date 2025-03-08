650407a1456442d40a25f244fa030e2768801ca2
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-03-08 19:12:19 +0530

    2379. Minimum Recolors to Get K Consecutive Black Blocks

--- March2025Challenge/March8Problem2379MinimumRecolorstoGetKConsecutiveBlackBlocks.java
+++ March2025Challenge/March8Problem2379MinimumRecolorstoGetKConsecutiveBlackBlocks.java
@@ -1,3 +1,5 @@
+package March2025Challenge;
+
 /* 2379. [Easy] Minimum Recolors to Get K Consecutive Black Blocks */
 public class March8Problem2379MinimumRecolorstoGetKConsecutiveBlackBlocks {
     public static void main(String[] args) {
