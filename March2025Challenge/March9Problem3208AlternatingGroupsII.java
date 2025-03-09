08b764899233eeaba522731b16421db3338bcf61
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-03-09 13:09:01 +0530

    3208. Alternating Groups II

--- March2025Challenge/March9Problem3208AlternatingGroupsII.java
+++ March2025Challenge/March9Problem3208AlternatingGroupsII.java
@@ -4,7 +4,7 @@ public class March9Problem3208AlternatingGroupsII {
     public static void main(String[] args) {
         
     }
-    public int numberOfAlternatingGroups(int[] colors, int k) {
+    public static int numberOfAlternatingGroups(int[] colors, int k) {
         int n = colors.length;
         int[] temp = new int[n + k - 1];
         System.arraycopy(colors, 0, temp, 0, n);
