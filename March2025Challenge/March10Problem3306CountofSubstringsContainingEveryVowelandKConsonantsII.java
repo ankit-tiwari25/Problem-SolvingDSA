d6a7911accfe856d40076820d9dc45167695d951
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-03-10 17:37:24 +0530

    3306. Count of Substrings Containing Every Vowel and K Consonants II

--- March2025Challenge/March10Problem3306CountofSubstringsContainingEveryVowelandKConsonantsII.java
+++ March2025Challenge/March10Problem3306CountofSubstringsContainingEveryVowelandKConsonantsII.java
@@ -3,6 +3,8 @@ package March2025Challenge;
 /* 3306. [Medium] Count of Substrings Containing Every Vowel and K Consonants II */
 public class March10Problem3306CountofSubstringsContainingEveryVowelandKConsonantsII {
     public static void main(String[] args) {
+        String word = "aeiou";
+        int  k = 0;
         
     }
     public static long countOfSubstrings(String word, int k) {
