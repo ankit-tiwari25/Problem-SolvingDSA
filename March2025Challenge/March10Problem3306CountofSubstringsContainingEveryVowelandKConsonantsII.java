656d5a2a85484e17224f74cd8bd191cae91fc350
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-03-10 17:34:17 +0530

    3306. Count of Substrings Containing Every Vowel and K Consonants II

--- March2025Challenge/March10Problem3306CountofSubstringsContainingEveryVowelandKConsonantsII.java
+++ March2025Challenge/March10Problem3306CountofSubstringsContainingEveryVowelandKConsonantsII.java
@@ -2,5 +2,7 @@ package March2025Challenge;
 
 /* 3306. [Medium] Count of Substrings Containing Every Vowel and K Consonants II */
 public class March10Problem3306CountofSubstringsContainingEveryVowelandKConsonantsII {
-    
+    public static void main(String[] args) {
+        
+    }
 }
