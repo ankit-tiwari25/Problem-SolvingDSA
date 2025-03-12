2db70951c26b991b434e94e423ad8e48e14b75a9
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-03-12 19:18:50 +0530

    2529. Maximum Count of Positive Integer and Negative Integer

--- March2025Challenge/March12Problem2529MaximumCountofPositiveIntegerandNegativeInteger.java
+++ March2025Challenge/March12Problem2529MaximumCountofPositiveIntegerandNegativeInteger.java
@@ -9,4 +9,19 @@ public class March12Problem2529MaximumCountofPositiveIntegerandNegativeInteger {
       int posCount = nums.length - binarySearch(nums, 1);
       return Math.max(negCount, posCount);
   }
+  private static int binarySearch(int[] nums, int target) {
+    int left = 0, right = nums.length - 1, result = nums.length;
+    
+    while (left <= right) {
+        int mid = (left + right) / 2;
+        if (nums[mid] < target) {
+            left = mid + 1;
+        } else {
+            result = mid;
+            right = mid - 1;
+        }
+    }
+    
+    return result;
+}
 }
