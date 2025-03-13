f4490da3659be4eebdf1434d8a91b1fd62d18dff
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-03-13 19:22:42 +0530

    3356. Zero Array Transformation II

--- March2025Challenge/March13Problem3356ZeroArrayTransformationII.java
+++ March2025Challenge/March13Problem3356ZeroArrayTransformationII.java
@@ -1,5 +1,20 @@
 package March2025Challenge;
+
+import java.util.Arrays;
+
 /* 3356.[Medium] Zero Array Transformation II */
 public class March13Problem3356ZeroArrayTransformationII {
-    
+     public int minZeroArray(int[] nums, int[][] queries) {
+         int n = nums.length;
+        
+        if (Arrays.stream(nums).allMatch(x -> x == 0)) return 0;
+        int left = 1, right = queries.length;
+        if (!canMakeZeroArray(right, nums, queries)) return -1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (canMakeZeroArray(mid, nums, queries)) right = mid;
+            else left = mid + 1;
+        }
+        return left;
+    }
 }
