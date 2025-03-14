f3bd50b64607058490031118e1bd1cab7023671c
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-03-14 19:30:00 +0530

    2226. Maximum Candies Allocated to K Children

---
+++ March2025Challenge/March14Problem2226MaximumCandiesAllocatedtoKChildren.java
@@ -0,0 +1,29 @@
+package March2025Challenge;
+/* 2226. [Medium] Maximum Candies Allocated to K Children */
+public class March14Problem2226MaximumCandiesAllocatedtoKChildren {
+    public static void main(String[] args) {
+        
+    }
+    public static int maximumCandies(int[] candies, long k) {
+        int left = 1, right = 10_000_000;
+         int result = 0;
+ 
+         while (left <= right) {
+             int mid = left + (right - left) / 2;
+             long childrenCount = 0;
+ 
+             for (int candy : candies) {
+                 childrenCount += candy / mid;
+             }
+ 
+             if (childrenCount >= k) {
+                 result = mid;
+                 left = mid + 1;
+             } else {
+                 right = mid - 1;
+             }
+         }
+ 
+         return result;  
+     }
+}
