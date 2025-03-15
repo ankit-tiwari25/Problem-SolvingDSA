9926b460c9bb8f484a119f89c665c4037e27d226
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-03-15 13:32:37 +0530

    2560. House Robber IV

--- March2025Challenge/March15Problem2560HouseRobberIV.java
+++ March2025Challenge/March15Problem2560HouseRobberIV.java
@@ -2,5 +2,17 @@ package March2025Challenge;
 
 /* 2560.[Medium] House Robber IV */
 public class March15Problem2560HouseRobberIV {
-    
+    private boolean canStealKHouses(int[] nums, int k, int capability) {
+        int count = 0;
+        int i = 0;
+        while (i < nums.length) {
+            if (nums[i] <= capability) {
+                count++;
+                i += 2;
+            } else {
+                i++;
+            }
+        }
+        return count >= k;
+    }
 }
