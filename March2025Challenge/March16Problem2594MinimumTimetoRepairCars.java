289e2b433dfc8c06811a9cad2bc2dc46adf9942d
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-03-16 20:19:00 +0530

    2594. Minimum Time to Repair Cars

--- March2025Challenge/March16Problem2594MinimumTimetoRepairCars.java
+++ March2025Challenge/March16Problem2594MinimumTimetoRepairCars.java
@@ -1,7 +1,7 @@
 package March2025Challenge;
 /* 2594.[Medium] Minimum Time to Repair Cars */
 public class March16Problem2594MinimumTimetoRepairCars {
-    public long repairCars(int[] ranks, int cars) {
+    public static long repairCars(int[] ranks, int cars) {
         long l = 1, r = (long) 1e14;
        while (l < r) {
            long mid = (l + r) / 2;
