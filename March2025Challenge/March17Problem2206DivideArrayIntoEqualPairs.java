eae366f37f6d94a15d189f758905ae567a189dd2
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-03-17 21:55:03 +0530

    2206. Divide Array Into Equal Pairs

--- March2025Challenge/March17Problem2206DivideArrayIntoEqualPairs.java
+++ March2025Challenge/March17Problem2206DivideArrayIntoEqualPairs.java
@@ -5,7 +5,8 @@ import java.util.Arrays;
 /* 2206.[Easy] Divide Array Into Equal Pairs */
 public class March17Problem2206DivideArrayIntoEqualPairs {
     public static void main(String[] args) {
-        
+        int[] nums = {3,2,3,2,2,2};
+        System.out.println(nums);
     }
     public static boolean divideArray(int[] nums) {
         Arrays.sort(nums);
