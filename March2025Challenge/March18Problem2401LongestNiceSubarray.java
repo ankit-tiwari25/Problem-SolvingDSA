324eaac77bf3a7230704454c1a3bc32179006278
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-03-18 15:56:15 +0530

    2401. Longest Nice Subarray

--- March2025Challenge/March18Problem2401LongestNiceSubarray.java
+++ March2025Challenge/March18Problem2401LongestNiceSubarray.java
@@ -5,4 +5,22 @@ public class March18Problem2401LongestNiceSubarray {
     public static void main(String[] args) {
         
     }
+    public int longestNiceSubarray(int[] nums) {
+        int n = nums.length;
+          int maxLength = 1;
+          int left = 0;
+          int usedBits = 0;
+          
+          for (int right = 0; right < n; right++) {
+              while ((usedBits & nums[right]) != 0) {
+                  usedBits ^= nums[left];
+                  left++;
+              }
+              
+              usedBits |= nums[right];
+              maxLength = Math.max(maxLength, right - left + 1);
+          }
+          
+          return maxLength;   
+      }
 }
