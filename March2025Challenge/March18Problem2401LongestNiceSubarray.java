a7ac42c909810b7817977d2e9527b1aa30a832ec
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-03-18 15:56:00 +0530

    2401. Longest Nice Subarray

--- March2025Challenge/March18Problem2401LongestNiceSubarray.java
+++ March2025Challenge/March18Problem2401LongestNiceSubarray.java
@@ -2,5 +2,7 @@ package March2025Challenge;
 
 /* 2401.[Medium] Longest Nice Subarray */
 public class March18Problem2401LongestNiceSubarray {
-    
+    public static void main(String[] args) {
+        
+    }
 }
