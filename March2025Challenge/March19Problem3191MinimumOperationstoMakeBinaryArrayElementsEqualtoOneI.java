6dca8abe441d3b7ad5ce9d771ca843d51d0400a7
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-03-19 22:44:33 +0530

    3191. Minimum Operations to Make Binary Array Elements Equal to One I

--- March2025Challenge/March19Problem3191MinimumOperationstoMakeBinaryArrayElementsEqualtoOneI.java
+++ March2025Challenge/March19Problem3191MinimumOperationstoMakeBinaryArrayElementsEqualtoOneI.java
@@ -2,7 +2,7 @@ package March2025Challenge;
 /* 3191.[Medium] Minimum Operations to Make Binary Array Elements Equal to One I */
 public class March19Problem3191MinimumOperationstoMakeBinaryArrayElementsEqualtoOneI {
     public static void main(String[] args) {
-        
+        int[]  nums = {0,1,1,1,0,0};
     }
     public static int minOperations(int[] nums) {
         int count = 0;
