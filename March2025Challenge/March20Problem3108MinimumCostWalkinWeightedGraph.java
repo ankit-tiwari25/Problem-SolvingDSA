2c52791386e4c917707fa4be318e6fbc6c07c596
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-03-20 08:02:30 +0530

    3108. Minimum Cost Walk in Weighted Graph

--- March2025Challenge/March20Problem3108MinimumCostWalkinWeightedGraph.java
+++ March2025Challenge/March20Problem3108MinimumCostWalkinWeightedGraph.java
@@ -4,4 +4,10 @@ public class March20Problem3108MinimumCostWalkinWeightedGraph {
     public static void main(String[] args) {
         
     }
+    private static int findRoot(int[] parent, int node) {
+        if (parent[node] != node) {
+            parent[node] = findRoot(parent, parent[node]);
+        }
+        return parent[node];
+    }
 }
