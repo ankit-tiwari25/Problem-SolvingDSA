fc8c53e661ec9042639deab3a4787780a651f92e
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-03-21 18:43:37 +0530

    2115. Find All Possible Recipes from Given Supplies

--- March2025Challenge/March21Problem2115FindAllPossibleRecipesfromGivenSupplies.java
+++ March2025Challenge/March21Problem2115FindAllPossibleRecipesfromGivenSupplies.java
@@ -1,6 +1,29 @@
 /*  2115.[Medium] Find All Possible Recipes from Given Supplies */
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+
 public class March21Problem2115FindAllPossibleRecipesfromGivenSupplies {
     public static void main(String[] args) {
         
+    }
+    public static List<String> findAllRecipes(String[] recipes, List<List<String>> ingredients, String[] supplies) {
+        availableSupplies = new HashSet<>(Arrays.asList(supplies));
+        recipeToIngredients = new HashMap<>();
+        visited = new HashMap<>();
+        result = new ArrayList<>();
+
+        for (int i = 0; i < recipes.length; i++) {
+            recipeToIngredients.put(recipes[i], ingredients.get(i));
+        }
+
+        for (String recipe : recipes) {
+            canMake(recipe);
+        }
+
+        return result;
     }
 }
