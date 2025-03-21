496410908117f440a6e1519c32f78a2dfbd43f47
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-03-21 18:42:49 +0530

    2115. Find All Possible Recipes from Given Supplies

--- March2025Challenge/March21Problem2115FindAllPossibleRecipesfromGivenSupplies.java
+++ March2025Challenge/March21Problem2115FindAllPossibleRecipesfromGivenSupplies.java
@@ -1,4 +1,6 @@
 /*  2115.[Medium] Find All Possible Recipes from Given Supplies */
 public class March21Problem2115FindAllPossibleRecipesfromGivenSupplies {
-    
+    public static void main(String[] args) {
+        
+    }
 }
