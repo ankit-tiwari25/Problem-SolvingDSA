fcb7e4f82acb6f84a25553bf1ba0842b36850e22
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-03-23 20:01:39 +0530

    1976. Number of Ways to Arrive at Destination

--- March2025Challenge/March23Problem1976NumberofWaystoArriveatDestination.java
+++ March2025Challenge/March23Problem1976NumberofWaystoArriveatDestination.java
@@ -1,4 +1,6 @@
 /* 1976.[Medium] Number of Ways to Arrive at Destination */
 public class March23Problem1976NumberofWaystoArriveatDestination {
-    
+    public static void main(String[] args) {
+        
+    }
 }
