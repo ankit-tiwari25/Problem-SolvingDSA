cd668a6325d254ad94504d3452b9b29df4408b3c
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-03-25 21:21:40 +0530

    3394. Check if Grid can be Cut into Sections

--- March2025Challenge/March25Problem3394CheckifGridcanbeCutintoSections.java
+++ March2025Challenge/March25Problem3394CheckifGridcanbeCutintoSections.java
@@ -4,4 +4,17 @@ public class March25Problem3394CheckifGridcanbeCutintoSections {
     public static void main(String[] args) {
         
     }
+    public boolean checkValidCuts(int n, int[][] rectangles) {
+        int[][] xIntervals = new int[rectangles.length][2];
+         int[][] yIntervals = new int[rectangles.length][2];
+ 
+         for (int i = 0; i < rectangles.length; i++) {
+             xIntervals[i][0] = rectangles[i][0];
+             xIntervals[i][1] = rectangles[i][2];
+             yIntervals[i][0] = rectangles[i][1];
+             yIntervals[i][1] = rectangles[i][3];
+         }
+ 
+         return check(xIntervals) || check(yIntervals); 
+     }
 }
