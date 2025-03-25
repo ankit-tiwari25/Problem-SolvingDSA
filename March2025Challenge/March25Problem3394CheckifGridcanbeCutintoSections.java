9840a1f755bebfd983cebb423923c3058ca789f0
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-03-25 21:21:25 +0530

    3394. Check if Grid can be Cut into Sections

--- March2025Challenge/March25Problem3394CheckifGridcanbeCutintoSections.java
+++ March2025Challenge/March25Problem3394CheckifGridcanbeCutintoSections.java
@@ -1,5 +1,7 @@
 
 /* 3394. [Medium] Check if Grid can be Cut into Sections */
 public class March25Problem3394CheckifGridcanbeCutintoSections {
-    
+    public static void main(String[] args) {
+        
+    }
 }
