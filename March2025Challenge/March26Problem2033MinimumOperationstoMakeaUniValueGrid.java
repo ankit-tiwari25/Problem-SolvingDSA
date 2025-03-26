e44f1a9d1e3aa15b6f7bbf249f883b4d06632a9d
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-03-26 22:49:58 +0530

    2033. Minimum Operations to Make a Uni-Value Grid

--- March2025Challenge/March26Problem2033MinimumOperationstoMakeaUniValueGrid.java
+++ March2025Challenge/March26Problem2033MinimumOperationstoMakeaUniValueGrid.java
@@ -1,6 +1,37 @@
 /* 2033.[Medium] Minimum Operations to Make a Uni-Value Grid */
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class March26Problem2033MinimumOperationstoMakeaUniValueGrid {
     public static void main(String[] args) {
         
+    }
+     public int minOperations(int[][] grid, int x) {
+        List<Integer> values = new ArrayList<>();
+        
+        for (int[] row : grid) {
+            for (int val : row) {
+                values.add(val);
+            }
+        }
+
+        Collections.sort(values);
+
+        for (int val : values) {
+            if (Math.abs(val - values.get(0)) % x != 0) {
+                return -1;
+            }
+        }
+
+        int median = values.get(values.size() / 2);
+        int operations = 0;
+
+        for (int val : values) {
+            operations += Math.abs(val - median) / x;
+        }
+
+        return operations; 
     }
 }
