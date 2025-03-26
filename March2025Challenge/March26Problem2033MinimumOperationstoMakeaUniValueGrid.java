7b402231df42758b53e528fa9f68b3dbd905daea
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-03-26 22:49:34 +0530

    2033. Minimum Operations to Make a Uni-Value Grid

--- March2025Challenge/March26Problem2033MinimumOperationstoMakeaUniValueGrid.java
+++ March2025Challenge/March26Problem2033MinimumOperationstoMakeaUniValueGrid.java
@@ -1,3 +1,6 @@
+/* 2033.[Medium] Minimum Operations to Make a Uni-Value Grid */
 public class March26Problem2033MinimumOperationstoMakeaUniValueGrid {
-    
+    public static void main(String[] args) {
+        
+    }
 }
