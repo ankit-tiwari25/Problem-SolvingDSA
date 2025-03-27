40f6a200caa293ffca52fc10947ae40d402d8764
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-03-27 20:13:43 +0530

    2780. Minimum Index of a Valid Split

--- March2025Challenge/March27Problem2780MinimumIndexofaValidSplit.java
+++ March2025Challenge/March27Problem2780MinimumIndexofaValidSplit.java
@@ -1,7 +1,41 @@
 
 /* 2780.[Medium] Minimum Index of a Valid Split */
+
+import java.util.List;
+
 public class March27Problem2780MinimumIndexofaValidSplit {
     public static void main(String[] args) {
         
+    }
+    public int minimumIndex(List<Integer> list) {
+        int n = list.size();
+        int max = Integer.MIN_VALUE;
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (count == 0)
+                max = list.get(i);
+            if (max == list.get(i))
+                count++;
+            else
+                count--;
+        }
+        count = 0;
+        for (int num : list) {
+            if (num == max)
+                count++;
+        }
+        if (count <= (n - count) + 1)
+            return -1;
+
+        count = 0;
+        for (int i = 0; i < n; i++) {
+            if (list.get(i) == max)
+                count++;
+            else
+                count--;
+            if (count == 1)
+                return i;
+        }
+        return -1;
     }
 }
