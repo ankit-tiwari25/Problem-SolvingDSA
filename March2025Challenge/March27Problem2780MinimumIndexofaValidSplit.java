5f601ebc61fac80a0ec260c4b238d29052413a54
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-03-27 20:13:29 +0530

    2780. Minimum Index of a Valid Split

--- March2025Challenge/March27Problem2780MinimumIndexofaValidSplit.java
+++ March2025Challenge/March27Problem2780MinimumIndexofaValidSplit.java
@@ -1,5 +1,7 @@
 
 /* 2780.[Medium] Minimum Index of a Valid Split */
 public class March27Problem2780MinimumIndexofaValidSplit {
-    
+    public static void main(String[] args) {
+        
+    }
 }
