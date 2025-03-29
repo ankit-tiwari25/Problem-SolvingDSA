0a1096861af54ef98931c3018b9d11802342894a
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-03-29 15:13:18 +0530

    2818. Apply Operations to Maximize Score

--- March2025Challenge/March29Problem2818ApplyOperationstoMaximizeScore.java
+++ March2025Challenge/March29Problem2818ApplyOperationstoMaximizeScore.java
@@ -1,6 +1,60 @@
 
 /* 2818.[Hard] Apply Operations to Maximize Score */
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.List;
+
 public class March29Problem2818ApplyOperationstoMaximizeScore {
     private final int mod = (int) 1e9 + 7;
 
+     public int maximumScore(List<Integer> nums, int k) {
+      int n = nums.size();
+        int[][] arr = new int[n][0];
+        for (int i = 0; i < n; ++i) {
+            arr[i] = new int[] {i, primeFactors(nums.get(i)), nums.get(i)};
+        }
+        int[] left = new int[n];
+        int[] right = new int[n];
+        Arrays.fill(left, -1);
+        Arrays.fill(right, n);
+        Deque<Integer> stk = new ArrayDeque<>();
+        for (int[] e : arr) {
+            int i = e[0], f = e[1];
+            while (!stk.isEmpty() && arr[stk.peek()][1] < f) {
+                stk.pop();
+            }
+            if (!stk.isEmpty()) {
+                left[i] = stk.peek();
+            }
+            stk.push(i);
+        }
+        stk.clear();
+        for (int i = n - 1; i >= 0; --i) {
+            int f = arr[i][1];
+            while (!stk.isEmpty() && arr[stk.peek()][1] <= f) {
+                stk.pop();
+            }
+            if (!stk.isEmpty()) {
+                right[i] = stk.peek();
+            }
+            stk.push(i);
+        }
+        Arrays.sort(arr, (a, b) -> b[2] - a[2]);
+        long ans = 1;
+        for (int[] e : arr) {
+            int i = e[0], x = e[2];
+            int l = left[i], r = right[i];
+            long cnt = (long) (i - l) * (r - i);
+            if (cnt <= k) {
+                ans = ans * qpow(x, cnt) % mod;
+                k -= cnt;
+            } else {
+                ans = ans * qpow(x, k) % mod;
+                break;
+            }
+        }
+        return (int) ans;   
+    }
 }
