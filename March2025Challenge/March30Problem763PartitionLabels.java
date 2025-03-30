87cc1f7cb7f1776a44bd3997113a43371c63f501
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-03-30 12:20:46 +0530

    763. Partition Labels

--- March2025Challenge/March30Problem763PartitionLabels.java
+++ March2025Challenge/March30Problem763PartitionLabels.java
@@ -1,6 +1,31 @@
 /* 763.[Medium] Partition Labels */
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class March30Problem763PartitionLabels {
     public static void main(String[] args) {
         
+    }
+     public List<Integer> partitionLabels(String s) {
+         Map<Character, Integer> lastOccurrence = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            lastOccurrence.put(s.charAt(i), i);
+        }
+
+        List<Integer> result = new ArrayList<>();
+        int start = 0, end = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            end = Math.max(end, lastOccurrence.get(s.charAt(i)));
+            if (i == end) {
+                result.add(end - start + 1);
+                start = i + 1;
+            }
+        }
+
+        return result;
     }
 }
