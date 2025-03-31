3d08eb3c9013d78919f2103ecaa06a81252b34e6
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-03-31 14:18:38 +0530

    2551.[Hard] Put Marbles in Bags

--- March2025Challenge/March31Problem2551PutMarblesinBags.java
+++ March2025Challenge/March31Problem2551PutMarblesinBags.java
@@ -1,6 +1,31 @@
 /* 2551.[Hard] Put Marbles in Bags */
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class March31Problem2551PutMarblesinBags {
     public static void main(String[] args) {
         
+    }
+      public long putMarbles(int[] weights, int k) {
+      if (k == 1) {
+            return 0;
+        }
+
+        List<Integer> pairSums = new ArrayList<>();
+        for (int i = 0; i < weights.length - 1; i++) {
+            pairSums.add(weights[i] + weights[i + 1]);
+        }
+
+        Collections.sort(pairSums);
+
+        long minScore = 0, maxScore = 0;
+        for (int i = 0; i < k - 1; i++) {
+            minScore += pairSums.get(i);
+            maxScore += pairSums.get(pairSums.size() - 1 - i);
+        }
+
+        return maxScore - minScore;  
     }
 }
