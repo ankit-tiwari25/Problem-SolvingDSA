b55eda7edd72eb1787aeecd5fcb60c1dcaba7077
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-04-01 14:09:44 +0530

    2140. Solving Questions With Brainpower

--- AprilChallenge2025/April1Problem2140SolvingQuestionsWithBrainpower.java
+++ AprilChallenge2025/April1Problem2140SolvingQuestionsWithBrainpower.java
@@ -7,7 +7,7 @@ public class April1Problem2140SolvingQuestionsWithBrainpower {
     public static void main(String[] args) {
         
     }
-    public long mostPoints(int[][] questions) {
+    public static long mostPoints(int[][] questions) {
          int n = questions.length;
         long[] dp = new long[n];
         Arrays.fill(dp,-1);
