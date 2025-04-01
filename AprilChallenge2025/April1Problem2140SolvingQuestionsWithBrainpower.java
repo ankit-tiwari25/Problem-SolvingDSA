c1c0e957d34a00599696398f485deb18bb03c69b
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-04-01 14:09:20 +0530

    2140. Solving Questions With Brainpower

--- AprilChallenge2025/April1Problem2140SolvingQuestionsWithBrainpower.java
+++ AprilChallenge2025/April1Problem2140SolvingQuestionsWithBrainpower.java
@@ -1,5 +1,7 @@
 package AprilChallenge2025;
 /* 2140.[Medium] Solving Questions With Brainpower */
 public class April1Problem2140SolvingQuestionsWithBrainpower {
-    
+    public static void main(String[] args) {
+        
+    }
 }
