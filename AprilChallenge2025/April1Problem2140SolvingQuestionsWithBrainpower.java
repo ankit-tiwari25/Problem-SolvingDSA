3928627d960caef58d3fbe4cd54f266755a6c0b1
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-04-01 14:09:56 +0530

    2140. Solving Questions With Brainpower

--- AprilChallenge2025/April1Problem2140SolvingQuestionsWithBrainpower.java
+++ AprilChallenge2025/April1Problem2140SolvingQuestionsWithBrainpower.java
@@ -13,4 +13,15 @@ public class April1Problem2140SolvingQuestionsWithBrainpower {
         Arrays.fill(dp,-1);
         return rec(0,questions,dp,n); 
     }
+    private long rec(int i,int[][] questions,long[] dp,int n)
+    {
+        if(i>=n) return 0;
+        if(dp[i]!=-1) return dp[i];
+
+        long take = questions[i][0] + rec(i+questions[i][1]+1,questions,dp,n);
+        long dont = rec(i+1,questions,dp,n);
+
+        return dp[i] = Math.max(take,dont);
+
+    }
 }
