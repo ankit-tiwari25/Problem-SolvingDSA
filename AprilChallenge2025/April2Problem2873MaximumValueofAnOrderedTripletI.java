a53d6cf2dfb6757d19e437654b8b6416bfc76dfa
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-04-02 20:00:18 +0530

    2873. Maximum Value of an Ordered Triplet I

--- AprilChallenge2025/April2Problem2873MaximumValueofAnOrderedTripletI.java
+++ AprilChallenge2025/April2Problem2873MaximumValueofAnOrderedTripletI.java
@@ -1,5 +1,7 @@
 package AprilChallenge2025;
 /* 2873.[Easy] Maximum Value of an Ordered Triplet I */
 public class April2Problem2873MaximumValueofAnOrderedTripletI {
-    
+    public static void main(String[] args) {
+        
+    }
 }
