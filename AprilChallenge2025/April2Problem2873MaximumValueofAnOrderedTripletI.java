cb46eed90eedf8e0dda46891f9365b4f32b934f5
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-04-02 20:02:25 +0530

    2873. Maximum Value of an Ordered Triplet I

--- AprilChallenge2025/April2Problem2873MaximumValueofAnOrderedTripletI.java
+++ AprilChallenge2025/April2Problem2873MaximumValueofAnOrderedTripletI.java
@@ -3,7 +3,7 @@ package AprilChallenge2025;
 public class April2Problem2873MaximumValueofAnOrderedTripletI {
     public static void main(String[] args) {
        int[] nums = {12,6,1,2,7};
-       
+       System.out.println(maximumTripletValue(nums));
     }
     public static long maximumTripletValue(int[] nums) {
         long maxTriplet = 0;
