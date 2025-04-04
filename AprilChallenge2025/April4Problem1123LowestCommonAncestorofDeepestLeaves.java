962c6b46f63a6300f3f41f7c3d380df6a448822d
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-04-04 16:02:35 +0530

    1123. Lowest Common Ancestor of Deepest Leaves

--- AprilChallenge2025/April4Problem1123LowestCommonAncestorofDeepestLeaves.java
+++ AprilChallenge2025/April4Problem1123LowestCommonAncestorofDeepestLeaves.java
@@ -4,4 +4,8 @@ public class April4Problem1123LowestCommonAncestorofDeepestLeaves {
     public static void main(String[] args) {
         
     }
+    private int maxDepth(TreeNode root) {
+        if (root == null) return 0;
+        return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+    }
 }
