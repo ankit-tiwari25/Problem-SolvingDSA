295f6571c8c158f14cc8de21f16f8af4692f1987
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-04-05 19:23:22 +0530

    1863. Sum of All Subset XOR Totals

--- AprilChallenge2025/April5Problem1863SumofAllSubsetXORTotals.java
+++ AprilChallenge2025/April5Problem1863SumofAllSubsetXORTotals.java
@@ -1,5 +1,7 @@
 package AprilChallenge2025;
 /* 1863.[Easy] Sum of All Subset XOR Totals */
 public class April5Problem1863SumofAllSubsetXORTotals {
-    
+    public static void main(String[] args) {
+        
+    }
 }
