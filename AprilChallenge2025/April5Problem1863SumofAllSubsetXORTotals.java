61c7c15af2e1fb85f77b8e560c04c7644b420b48
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-04-05 19:23:39 +0530

    1863. Sum of All Subset XOR Totals

--- AprilChallenge2025/April5Problem1863SumofAllSubsetXORTotals.java
+++ AprilChallenge2025/April5Problem1863SumofAllSubsetXORTotals.java
@@ -4,4 +4,11 @@ public class April5Problem1863SumofAllSubsetXORTotals {
     public static void main(String[] args) {
         
     }
+    public int subsetXORSum(int[] nums) {
+        int total = 0;
+         for (int num : nums) {
+             total |= num;  // Step 1: Compute bitwise OR of all numbers
+         }
+         return total * (1 << (nums.length - 1));  
+     }
 }
