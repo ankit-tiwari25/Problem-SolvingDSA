5b56e14705077b581594a38877b238f4c0295beb
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-04-07 17:51:24 +0530

    416. Partition Equal Subset Sum

--- AprilChallenge2025/April7Problem416PartitionEqualSubsetSum.java
+++ AprilChallenge2025/April7Problem416PartitionEqualSubsetSum.java
@@ -1,3 +1,5 @@
+package AprilChallenge2025;
+/* 416.[Medium] Partition Equal Subset Sum */
 public class April7Problem416PartitionEqualSubsetSum {
     public static void main(String[] args) {
         
