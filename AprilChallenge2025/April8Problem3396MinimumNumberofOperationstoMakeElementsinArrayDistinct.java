799b3222c8b0b8e357e08a1b1001b994e759a90f
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-04-08 23:46:10 +0530

    3396. Minimum Number of Operations to Make Elements in Array Distinct

--- AprilChallenge2025/April8Problem3396MinimumNumberofOperationstoMakeElementsinArrayDistinct.java
+++ AprilChallenge2025/April8Problem3396MinimumNumberofOperationstoMakeElementsinArrayDistinct.java
@@ -1,3 +1,4 @@
+/* 3396. [Easy] Minimum Number of Operations to Make Elements in Array Distinct */
 public class April8Problem3396MinimumNumberofOperationstoMakeElementsinArrayDistinct {
     public static void main(String[] args) {
         
