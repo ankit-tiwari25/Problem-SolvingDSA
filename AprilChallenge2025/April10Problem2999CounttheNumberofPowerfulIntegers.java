3cc5b3c4b9e12b4708ec9ba36a890d26dd37f02f
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-04-10 21:45:17 +0530

    2999. Count the Number of Powerful Integers

--- AprilChallenge2025/April10Problem2999CounttheNumberofPowerfulIntegers.java
+++ AprilChallenge2025/April10Problem2999CounttheNumberofPowerfulIntegers.java
@@ -4,4 +4,8 @@ public class April10Problem2999CounttheNumberofPowerfulIntegers {
     public static void main(String[] args) {
         
     }
+    static public long numberOfPowerfulInt(long start, long finish, int limit, String s) {
+        char[] suffixReq = s.toCharArray();
+        return result(finish, suffixReq, limit) - result(start - 1, suffixReq, limit);
+    }
 }
