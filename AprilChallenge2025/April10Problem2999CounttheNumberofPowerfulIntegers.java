c2de631aa785364f8cb6fe19e04dc11dcc3b850d
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-04-10 21:45:36 +0530

    2999. Count the Number of Powerful Integers

--- AprilChallenge2025/April10Problem2999CounttheNumberofPowerfulIntegers.java
+++ AprilChallenge2025/April10Problem2999CounttheNumberofPowerfulIntegers.java
@@ -1,4 +1,7 @@
 package AprilChallenge2025;
+
+import java.util.Arrays;
+
 /* 2999.[Hard] Count the Number of Powerful Integers */
 public class April10Problem2999CounttheNumberofPowerfulIntegers {
     public static void main(String[] args) {
@@ -8,4 +11,16 @@ public class April10Problem2999CounttheNumberofPowerfulIntegers {
         char[] suffixReq = s.toCharArray();
         return result(finish, suffixReq, limit) - result(start - 1, suffixReq, limit);
     }
+     static long result(long num, char[] sarr, int limit) {
+        char[] range = Long.toString(num).toCharArray();
+        int n = range.length, sLen = sarr.length;
+        Long[][] dp = new Long[n][2];
+        boolean checkIfSTrictThenPoss = false;
+        if (isSmaller(range, sarr)){
+            checkIfSTrictThenPoss = isSmaller(Arrays.copyOfRange(range, (n - sarr.length) , n), sarr);
+            return solve(0, 0, range, sarr, limit, n, dp, checkIfSTrictThenPoss, sLen);
+        }
+        return 0;
+    }
+
 }
