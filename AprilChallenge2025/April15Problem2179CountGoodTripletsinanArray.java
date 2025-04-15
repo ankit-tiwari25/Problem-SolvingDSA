b3d82bf3b5bd44eb40b7e462952010ebb7687559
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-04-15 23:18:36 +0530

    2179. Count Good Triplets in an Array

--- AprilChallenge2025/April15Problem2179CountGoodTripletsinanArray.java
+++ AprilChallenge2025/April15Problem2179CountGoodTripletsinanArray.java
@@ -1,7 +1,29 @@
 
 /* 2179. [Hard] Count Good Triplets in an Array */
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+
 public class April15Problem2179CountGoodTripletsinanArray {
     public static void main(String[] args) {
         
+    }
+    public long goodTriplets(int[] nums1, int[] nums2) {
+      HashMap<Integer, Integer> mpp = new HashMap<>();
+        for (int i = 0; i < nums1.length; i++) mpp.put(nums1[i], i);
+        int n = nums2.length;
+        long total = 0;
+        ArrayList<Integer> st = new ArrayList<>();
+        for (int x : nums2) {
+            int idx = mpp.get(x);
+            int left = orderOfKey(st, idx);
+            int right = (n - 1 - idx) - (st.size() - left);
+            total += (long) left * right;
+            int pos = Collections.binarySearch(st, idx);
+            if (pos < 0) pos = -pos - 1;
+            st.add(pos, idx);
+        }
+        return total;  
     }
 }
