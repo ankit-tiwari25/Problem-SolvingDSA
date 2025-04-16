57609c3a5c09b699d2c8666b232d9d0d57e2a232
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-04-16 17:00:24 +0530

    2537. Count the Number of Good Subarrays

--- AprilChallenge2025/April16Problem2537CounttheNumberofGoodSubarrays.java
+++ AprilChallenge2025/April16Problem2537CounttheNumberofGoodSubarrays.java
@@ -1,6 +1,29 @@
 /* 2537.[Medium] Count the Number of Good Subarrays */
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class April16Problem2537CounttheNumberofGoodSubarrays {
     public static void main(String[] args) {
         
+    }
+     public static long countGood(int[] nums, int k) {
+      Map<Integer, Integer> freq = new HashMap<>();
+        long prev_count = 0, res = 0;
+        int left = 0;
+        
+        for (int right = 0; right < nums.length; right++) {
+            prev_count += freq.getOrDefault(nums[right], 0);
+            freq.put(nums[right], freq.getOrDefault(nums[right], 0) + 1);
+            
+            while (prev_count >= k) {
+                res += nums.length - right;
+                prev_count -= freq.get(nums[left]) - 1;
+                freq.put(nums[left], freq.get(nums[left]) - 1);
+                left++;
+            }
+        }
+        
+        return res;  
     }
 }
