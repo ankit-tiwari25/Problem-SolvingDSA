c3f2997f4da6b844135a6bdc21e92a7d7724e574
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-04-17 23:31:37 +0530

    2176. Count Equal and Divisible Pairs in an Array

--- AprilChallenge2025/April17Problem2176CountEqualandDivisiblePairsinanArray.java
+++ AprilChallenge2025/April17Problem2176CountEqualandDivisiblePairsinanArray.java
@@ -1,3 +1,4 @@
+/* 2176.[Easy] Count Equal and Divisible Pairs in an Array */
 public class April17Problem2176CountEqualandDivisiblePairsinanArray {
     public static void main(String[] args) {
         
