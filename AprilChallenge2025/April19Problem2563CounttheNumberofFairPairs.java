e7a4704e1ba1510faa6b608c0d89a80aa4669167
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-04-19 15:28:57 +0530

    2563. Count the Number of Fair Pairs

--- AprilChallenge2025/April19Problem2563CounttheNumberofFairPairs.java
+++ AprilChallenge2025/April19Problem2563CounttheNumberofFairPairs.java
@@ -7,6 +7,7 @@ public class April19Problem2563CounttheNumberofFairPairs {
     public static void main(String[] args) {
       int[]  nums = {0,1,7,4,4,5};
       int lower = 3, upper = 6;
+      System.out.println(countAtLeast(nums, upper));
     }
     public static long countAtLeast(int[] nums, long comp) {
         long ans = 0; int i = 0, j = nums.length - 1;
