e6131f197d03698f66fa892e4aa5f6be445a856c
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-04-20 18:11:41 +0530

    781. Rabbits in Forest

--- AprilChallenge2025/April20Problem781RabbitsinForest.java
+++ AprilChallenge2025/April20Problem781RabbitsinForest.java
@@ -1,7 +1,29 @@
 
 /* 781.[Medium] Rabbits in Forest */
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class April20Problem781RabbitsinForest {
     public static void main(String[] args) {
         
+    }
+     public int numRabbits(int[] answers) {
+        Map<Integer, Integer> freq = new HashMap<>();
+
+        for (int a : answers) {
+            freq.put(a, freq.getOrDefault(a, 0) + 1);
+        }
+
+        int count = 0;
+        for (Map.Entry<Integer, Integer> entry : freq.entrySet()) {
+            int k = entry.getKey();
+            int v = entry.getValue();
+            int groupSize = k + 1;
+            int groups = (v + k) / groupSize;
+            count += groups * groupSize;
+        }
+
+        return count;
     }
 }
