549acd2a06e3557fa953b9431bee45ad2d7870b1
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-04-21 08:27:38 +0530

    2145. Count the Hidden Sequences

--- AprilChallenge2025/April21Problem2145CounttheHiddenSequences.java
+++ AprilChallenge2025/April21Problem2145CounttheHiddenSequences.java
@@ -3,6 +3,7 @@ public class April21Problem2145CounttheHiddenSequences {
     public static void main(String[] args) {
         int[] differences = {1,-3,4};
         int lower = 1, upper = 6;
+        
     }
     public static int numberOfArrays(int[] differences, int lower, int upper) {
         long sum = 0, maxi = 0, mini = 0;
