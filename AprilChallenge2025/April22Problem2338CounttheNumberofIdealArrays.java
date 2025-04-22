14e84971cbe7bef1418db38b4e6100ee832d0c31
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-04-22 09:12:26 +0530

    2338. Count the Number of Ideal Arrays

--- AprilChallenge2025/April22Problem2338CounttheNumberofIdealArrays.java
+++ AprilChallenge2025/April22Problem2338CounttheNumberofIdealArrays.java
@@ -1,5 +1,7 @@
 
 /* 2338.[Hard] Count the Number of Ideal Arrays */
 public class April22Problem2338CounttheNumberofIdealArrays {
-    
+    public static void main(String[] args) {
+        
+    }
 }
