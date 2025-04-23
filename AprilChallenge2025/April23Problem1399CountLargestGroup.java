1f2ae0b2b4570682c25b9a6104c11bb51c040021
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-04-23 20:09:52 +0530

    1399. Count Largest Group

--- AprilChallenge2025/April23Problem1399CountLargestGroup.java
+++ AprilChallenge2025/April23Problem1399CountLargestGroup.java
@@ -1,6 +1,32 @@
 /* 1399.[Easy] Count Largest Group */
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class April23Problem1399CountLargestGroup {
     public static void main(String[] args) {
         
+    }
+     public int countLargestGroup(int n) {
+        Map<Integer, Integer> mpp = new HashMap<>();
+        int maxi = 0, count = 0;
+
+        for (int i = 1; i <= n; i++) {
+            int x = digsum(i);
+            mpp.put(x, mpp.getOrDefault(x, 0) + 1);
+            maxi = Math.max(maxi, mpp.get(x));
+        }
+
+        for (int val : mpp.values()) if (val == maxi) count++;
+        return count;
+    }
+
+    private int digsum(int n) {
+        int sum = 0;
+        while (n > 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+        return sum;
     }
 }
