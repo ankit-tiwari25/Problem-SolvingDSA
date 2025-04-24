0b24b698af11d77bbfa63bdfe9dd1f903cde1558
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-04-24 20:35:55 +0530

    2799. Count Complete Subarrays in an Array

--- AprilChallenge2025/April24Problem2799CountCompleteSubarraysinanArray.java
+++ AprilChallenge2025/April24Problem2799CountCompleteSubarraysinanArray.java
@@ -7,7 +7,7 @@ import java.util.Map;
 public class April24Problem2799CountCompleteSubarraysinanArray {
     public static void main(String[] args) {
         int[]  nums = {1,3,1,2,2};
-        System.out.println();
+        System.out.println(countCompleteSubarrays(nums));
     }
      public static int countCompleteSubarrays(int[] nums) {
      int left = 0, res = 0;
