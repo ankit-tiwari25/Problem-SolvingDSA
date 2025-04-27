024ef85d75ee585f296784a78544d8c2b22a3681
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-04-27 15:29:07 +0530

    3392. Count Subarrays of Length Three With a Condition

--- AprilChallenge2025/April27Problem3392CountSubarraysofLengthThreeWithaCondition.java
+++ AprilChallenge2025/April27Problem3392CountSubarraysofLengthThreeWithaCondition.java
@@ -1,3 +1,4 @@
+/* 3392.[Easy] Count Subarrays of Length Three With a Condition */
 public class April27Problem3392CountSubarraysofLengthThreeWithaCondition {
     public static void main(String[] args) {
         
