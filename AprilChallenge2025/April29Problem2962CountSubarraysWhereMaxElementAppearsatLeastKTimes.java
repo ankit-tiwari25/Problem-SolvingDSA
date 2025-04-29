91846b773310cf82d2572e6ff1441d15de49d4ee
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-04-29 19:20:41 +0530

    2962. Count Subarrays Where Max Element Appears at Least K Times

--- AprilChallenge2025/April29Problem2962CountSubarraysWhereMaxElementAppearsatLeastKTimes.java
+++ AprilChallenge2025/April29Problem2962CountSubarraysWhereMaxElementAppearsatLeastKTimes.java
@@ -3,4 +3,20 @@ public class April29Problem2962CountSubarraysWhereMaxElementAppearsatLeastKTimes
     public static void main(String[] args) {
         
     }
+    public long countSubarrays(int[] nums, int k) {
+        int maxi = Integer.MIN_VALUE;
+         for (int num : nums) maxi = Math.max(maxi, num);
+         int left = 0;
+         long maxiOccurence = 0;
+         long res = 0;
+         for (int right = 0; right < nums.length; right++) {
+             if (nums[right] == maxi) maxiOccurence++;
+             while (maxiOccurence >= k) {
+                 if (nums[left] == maxi) maxiOccurence--;
+                 left++;
+             }
+             res += left;
+         }
+         return res;   
+     }
 }
