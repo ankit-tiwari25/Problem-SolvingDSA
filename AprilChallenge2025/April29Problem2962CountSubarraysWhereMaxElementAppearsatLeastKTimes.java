81b7d0d7125678b4f4a664f3b28d75e9267bbaf4
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-04-29 19:20:24 +0530

    2962. Count Subarrays Where Max Element Appears at Least K Times

--- AprilChallenge2025/April29Problem2962CountSubarraysWhereMaxElementAppearsatLeastKTimes.java
+++ AprilChallenge2025/April29Problem2962CountSubarraysWhereMaxElementAppearsatLeastKTimes.java
@@ -1,4 +1,4 @@
-/* 2962. Count Subarrays Where Max Element Appears at Least K Times */
+/* 2962. [Medium] Count Subarrays Where Max Element Appears at Least K Times */
 public class April29Problem2962CountSubarraysWhereMaxElementAppearsatLeastKTimes {
     public static void main(String[] args) {
         
