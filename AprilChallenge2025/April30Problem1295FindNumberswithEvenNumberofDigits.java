75a07c5f48931fec37267316e4896bb505251b8c
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-04-30 13:36:43 +0530

    1295. Find Numbers with Even Number of Digits

--- AprilChallenge2025/April30Problem1295FindNumberswithEvenNumberofDigits.java
+++ AprilChallenge2025/April30Problem1295FindNumberswithEvenNumberofDigits.java
@@ -1,4 +1,6 @@
 /* 1295.[Easy] Find Numbers with Even Number of Digits */
 public class April30Problem1295FindNumberswithEvenNumberofDigits {
-    
+    public static void main(String[] args) {
+        
+    }
 }
