24ec20a462f1243bd4c5d9f44d6af7d16705aa85
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-05-01 16:56:03 +0530

    2071. Maximum Number of Tasks You Can Assign

--- May2025/May1Problem2071MaximumNumberofTasksYouCanAssign.java
+++ May2025/May1Problem2071MaximumNumberofTasksYouCanAssign.java
@@ -5,6 +5,9 @@ import java.util.TreeMap;
 
 /* 2071.[Hard] Maximum Number of Tasks You Can Assign */
 public class May1Problem2071MaximumNumberofTasksYouCanAssign {
+    public static void main(String[] args) {
+        
+    }
      private static void decrement(TreeMap<Integer, Integer> m, int k) {
         int c = m.get(k);
         if (c == 1) m.remove(k);
