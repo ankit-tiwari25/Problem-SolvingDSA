4bc776885fb533eac7f01753f5967c578df2bcde
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-05-01 16:55:45 +0530

    2071. Maximum Number of Tasks You Can Assign

--- May2025/May1Problem2071MaximumNumberofTasksYouCanAssign.java
+++ May2025/May1Problem2071MaximumNumberofTasksYouCanAssign.java
@@ -1,5 +1,6 @@
 package May2025;
 
+import java.util.Arrays;
 import java.util.TreeMap;
 
 /* 2071.[Hard] Maximum Number of Tasks You Can Assign */
@@ -8,5 +9,41 @@ public class May1Problem2071MaximumNumberofTasksYouCanAssign {
         int c = m.get(k);
         if (c == 1) m.remove(k);
         else m.put(k, c - 1);
+    }
+    public int maxTaskAssign(int[] tasks, int[] workers, int pills, int strength) {
+       int left = 0, right = Math.min(tasks.length, workers.length);
+        Arrays.sort(tasks);
+        Arrays.sort(workers);
+
+        while (left < right) {
+            int mid = (left + right + 1) / 2;
+            int usedPills = 0;
+            TreeMap<Integer, Integer> avail = new TreeMap<>();
+            for (int i = workers.length - mid; i < workers.length; ++i)
+                avail.put(workers[i], avail.getOrDefault(workers[i], 0) + 1);
+
+            boolean canAssign = true;
+            for (int i = mid - 1; i >= 0; --i) {
+                int t = tasks[i];
+                int w = avail.lastKey();
+                if (w >= t) {
+                    decrement(avail, w);
+                } else {
+                    Integer key = avail.ceilingKey(t - strength);
+                    if (key == null || ++usedPills > pills) {
+                        canAssign = false;
+                        break;
+                    }
+                    decrement(avail, key);
+                }
+            }
+
+            if (canAssign)
+                left = mid;
+            else
+                right = mid - 1;
+        }
+
+        return left;  
     }
 }
