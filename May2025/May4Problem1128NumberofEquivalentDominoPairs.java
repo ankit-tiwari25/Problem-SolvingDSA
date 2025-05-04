fc84b40e5e2edf7a725bcc01b60d76aa3438255d
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-05-04 15:01:59 +0530

    1128. Number of Equivalent Domino Pairs

--- May2025/May4Problem1128NumberofEquivalentDominoPairs.java
+++ May2025/May4Problem1128NumberofEquivalentDominoPairs.java
@@ -1,5 +1,12 @@
 package May2025;
 /* 1128.[Easy] Number of Equivalent Domino Pairs */
 public class May4Problem1128NumberofEquivalentDominoPairs {
-    
+    public static void main(String[] args) {
+        
+    }
+    public long cantorPair(int a, int b) {
+        int x = Math.min(a, b), y = Math.max(a, b);
+        long sum = x + y;
+        return (sum * (sum + 1)) / 2 + y;
+    }
 }
