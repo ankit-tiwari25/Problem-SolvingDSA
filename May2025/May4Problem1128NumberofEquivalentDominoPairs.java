65669d5e24b72add2b5a0981c23c67a84bfaa926
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-05-04 15:02:18 +0530

    1128. Number of Equivalent Domino Pairs

--- May2025/May4Problem1128NumberofEquivalentDominoPairs.java
+++ May2025/May4Problem1128NumberofEquivalentDominoPairs.java
@@ -1,4 +1,8 @@
 package May2025;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /* 1128.[Easy] Number of Equivalent Domino Pairs */
 public class May4Problem1128NumberofEquivalentDominoPairs {
     public static void main(String[] args) {
@@ -8,5 +12,16 @@ public class May4Problem1128NumberofEquivalentDominoPairs {
         int x = Math.min(a, b), y = Math.max(a, b);
         long sum = x + y;
         return (sum * (sum + 1)) / 2 + y;
+    }
+
+    public int numEquivDominoPairs(int[][] dominoes) {
+        int count = 0;
+        Map<Long, Integer> freq = new HashMap<>();
+        for (int[] dom : dominoes) {
+            long pairValue = cantorPair(dom[0], dom[1]);
+            count += freq.getOrDefault(pairValue, 0);
+            freq.put(pairValue, freq.getOrDefault(pairValue, 0) + 1);
+        }
+        return count;
     }
 }
