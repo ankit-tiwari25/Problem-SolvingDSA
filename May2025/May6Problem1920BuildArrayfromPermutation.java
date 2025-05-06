60a329e9ce2cf20a3a6b0fb22f0f2e4c37ca01e6
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-05-06 19:22:13 +0530

    1920. Build Array from Permutation

--- May2025/May6Problem1920BuildArrayfromPermutation.java
+++ May2025/May6Problem1920BuildArrayfromPermutation.java
@@ -4,4 +4,12 @@ public class May6Problem1920BuildArrayfromPermutation {
     public static void main(String[] args) {
         
     }
+    public int[] buildArray(int[] nums) {
+        int[] arr = new int[nums.length];
+
+        for(int i = 0; i < nums.length; i++){
+            arr[i] = nums[nums[i]];
+        }
+        return arr;
+    }
 }
