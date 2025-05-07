22a7570373e8d81b0d9d98555c94f5fdb4ba79dc
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-05-07 19:28:01 +0530

    3341. Find Minimum Time to Reach Last Room I

--- May2025/May7Problem3341FindMinimumTimetoReachLastRoomI.java
+++ May2025/May7Problem3341FindMinimumTimetoReachLastRoomI.java
@@ -1,5 +1,7 @@
 package May2025;
 /* 3341.[Medium] Find Minimum Time to Reach Last Room I */
 public class May7Problem3341FindMinimumTimetoReachLastRoomI {
-    
+    public static void main(String[] args) {
+        
+    }
 }
