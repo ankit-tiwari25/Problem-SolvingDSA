104965bf4f42085af76b4f737e00a2c1ffa3b9a1
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-05-14 14:30:17 +0530

    3337. Total Characters in String After Transformations II

--- May2025/May14Problem3337TotalCharactersinStringAfterTransformationsII.java
+++ May2025/May14Problem3337TotalCharactersinStringAfterTransformationsII.java
@@ -1,4 +1,18 @@
 /* 3337.[Hard] Total Characters in String After Transformations II */
 public class May14Problem3337TotalCharactersinStringAfterTransformationsII {
      private static final int mod = 1000000007;
+      private static long[][] multiplyMatrices(long[][] A, long[][] B) {
+        int rowsA = A.length, colsA = A[0].length, colsB = B[0].length;
+        long[][] result = new long[rowsA][colsB];
+        for (int i = 0; i < rowsA; i++) {
+            for (int j = 0; j < colsB; j++) {
+                long sum = 0;
+                for (int k = 0; k < colsA; k++) {
+                    sum = (sum + (A[i][k] * B[k][j]) % mod) % mod;
+                }
+                result[i][j] = sum;
+            }
+        }
+        return result;
+    }
 }
