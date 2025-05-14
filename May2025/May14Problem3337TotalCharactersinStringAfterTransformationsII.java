cdf74992f57ba2177832656997a30a27df0659c1
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-05-14 14:29:58 +0530

    3337. Total Characters in String After Transformations II

--- May2025/May14Problem3337TotalCharactersinStringAfterTransformationsII.java
+++ May2025/May14Problem3337TotalCharactersinStringAfterTransformationsII.java
@@ -1,4 +1,4 @@
 /* 3337.[Hard] Total Characters in String After Transformations II */
 public class May14Problem3337TotalCharactersinStringAfterTransformationsII {
-    
+     private static final int mod = 1000000007;
 }
