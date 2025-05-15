8d7a4281023790d0e19660a50e9c173f677e7d3a
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-05-15 19:09:22 +0530

    2900. Longest Unequal Adjacent Groups Subsequence I

--- May2025/May15Problem2900LongestUnequalAdjacentGroupsSubsequenceI.java
+++ May2025/May15Problem2900LongestUnequalAdjacentGroupsSubsequenceI.java
@@ -1,3 +1,4 @@
+/* 2900. [Easy] Longest Unequal Adjacent Groups Subsequence I */
 public class May15Problem2900LongestUnequalAdjacentGroupsSubsequenceI {
     public static void main(String[] args) {
         
