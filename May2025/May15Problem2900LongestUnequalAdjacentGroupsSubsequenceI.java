10e461ea0b7a9d02ed3950b8e1ab1738a672acae
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-05-15 19:09:36 +0530

    2900. Longest Unequal Adjacent Groups Subsequence I

--- May2025/May15Problem2900LongestUnequalAdjacentGroupsSubsequenceI.java
+++ May2025/May15Problem2900LongestUnequalAdjacentGroupsSubsequenceI.java
@@ -1,6 +1,20 @@
 /* 2900. [Easy] Longest Unequal Adjacent Groups Subsequence I */
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class May15Problem2900LongestUnequalAdjacentGroupsSubsequenceI {
     public static void main(String[] args) {
         
+    }
+    public List<String> getLongestSubsequence(String[] words, int[] groups) {
+         List<String> res = new ArrayList<>();
+        res.add(words[0]);
+        for (int i = 1; i < groups.length; ++i) {
+            if (groups[i] != groups[i - 1]) {
+                res.add(words[i]);
+            }
+        }
+        return res; 
     }
 }
