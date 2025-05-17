cc08ae5ae9893c102e3b18983f387499e3897986
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-05-17 18:44:59 +0530

    75. Sort Colors

--- May2025/May17Problem75SortColors.java
+++ May2025/May17Problem75SortColors.java
@@ -1,6 +1,11 @@
 public class May17Problem75SortColors {
     public static void main(String[] args) {
-        
+        int[] nums = {2,0,2,1,1,0};
+        sortColors(nums);
+        for(int i : nums){
+            System.out.print(i+" ");
+        }
+        System.out.println();
     }
     public static void sortColors(int[] nums) {
        int low = 0, mid = 0, high = nums.length - 1;
