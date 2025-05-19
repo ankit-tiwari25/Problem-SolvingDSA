f7423e2a3dde0a6b58f541091ab831977e8f9b52
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-05-19 11:19:31 +0530

    3024. Type of Triangle

--- May2025/May19Problem3024TypeofTriangle.java
+++ May2025/May19Problem3024TypeofTriangle.java
@@ -1,6 +1,21 @@
 /* 3024.[Easy] Type of Triangle */
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class May19Problem3024TypeofTriangle {
     public static void main(String[] args) {
         
+    }
+    public String triangleType(int[] nums) {
+       Arrays.sort(nums);
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) set.add(num);
+
+        if (nums[0] + nums[1] <= nums[2]) return "none";
+        if (set.size() == 1) return "equilateral";
+        if (set.size() == 2) return "isosceles";
+        return "scalene";  
     }
 }
