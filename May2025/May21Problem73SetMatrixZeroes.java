1b96fe35451a18a0d009d1fabd9001934a48f79c
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-05-21 11:17:05 +0530

    73. Set Matrix Zeroes

--- May2025/May21Problem73SetMatrixZeroes.java
+++ May2025/May21Problem73SetMatrixZeroes.java
@@ -29,6 +29,11 @@ public class May21Problem73SetMatrixZeroes {
         }
 
         public static void display(int[][] mat){
-            
+            for(int i = 0; i < mat.length; i++){
+                for(int j = 0; j < mat[i].length; j++){
+                    System.out.print(mat[i][j] + " ");
+                }
+                System.out.println();
+            }
         }
 }
