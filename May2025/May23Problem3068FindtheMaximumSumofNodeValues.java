12ecd4899e10007310126c3070296401f76307e9
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-05-23 07:44:55 +0530

    3068. Find the Maximum Sum of Node Values

--- May2025/May23Problem3068FindtheMaximumSumofNodeValues.java
+++ May2025/May23Problem3068FindtheMaximumSumofNodeValues.java
@@ -4,4 +4,14 @@ public class May23Problem3068FindtheMaximumSumofNodeValues {
     public static void main(String[] args) {
         
     }
+    public long maximumValueSum(int[] nums, int k, int[][] edges) {
+        long[] dp = {0, Long.MIN_VALUE};
+        for (int n : nums) {
+            long cnt0 = dp[0] + n, cnt1 = dp[1] + n;
+            int x = n ^ k;
+            dp = new long[]{ Math.max(cnt0, dp[1] + x), 
+                             Math.max(cnt1, dp[0] + x) };
+        }
+        return dp[0]; 
+    }
 }
