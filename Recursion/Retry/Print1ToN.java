4f613c20e1ca2812747e3f722f535b022eb22996
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-05-24 10:23:48 +0530

    Practicing Recursion

---
+++ Recursion/Retry/Print1ToN.java
@@ -0,0 +1,16 @@
+package Recursion.Retry;
+/* IBH Method : Induction, Base Condition And Hypothesis */
+public class Print1ToN {
+    public static void main(String[] args) {
+        int n = 10;
+        print1toN(n);
+    }
+    public static void print1toN(int n){
+        /* Base condition */
+        if(n == 1){
+            System.out.print(n  + " ");
+        }
+        print1toN( n - 1);
+        System.out.print(" " + (n-1));
+    }
+}
