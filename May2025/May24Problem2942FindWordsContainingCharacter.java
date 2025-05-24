b49c9412403bfc8cf4b4ad342332f09bf1a73e1f
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-05-24 10:14:08 +0530

    2942. Find Words Containing Character

--- May2025/May24Problem2942FindWordsContainingCharacter.java
+++ May2025/May24Problem2942FindWordsContainingCharacter.java
@@ -1,6 +1,22 @@
 /* 2942. Find Words Containing Character */
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class May24Problem2942FindWordsContainingCharacter {
     public static void main(String[] args) {
         
+    }
+    public static List<Integer> findWordsContaining(String[] words, char x) {
+        List<Integer> ans =new ArrayList<>();
+
+        int i = 0;
+        for(String word : words){
+            if(word.contains(x+"")){
+                ans.add(i);
+            }
+            i++;
+        }
+        return ans;
     }
 }
