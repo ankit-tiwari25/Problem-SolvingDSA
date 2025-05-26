a621c01a12e54b7dcfabcad3ac50d359598f9c0a
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-05-26 19:00:36 +0530

    1857. Largest Color Value in a Directed Graph

--- May2025/May26Problem1857LargestColorValueinaDirectedGraph.java
+++ May2025/May26Problem1857LargestColorValueinaDirectedGraph.java
@@ -1,4 +1,6 @@
 /* 1857.[Hard] Largest Color Value in a Directed Graph */
 public class May26Problem1857LargestColorValueinaDirectedGraph {
-    
+    public static void main(String[] args) {
+        
+    }
 }
