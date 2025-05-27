eaa1ed39b8d276d484345c42a66247dbc14d59ba
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-05-27 18:44:41 +0530

    2894. Divisible and Non-divisible Sums Difference

--- May2025/May27Problem2894DivisibleandNondivisibleSumsDifference.java
+++ May2025/May27Problem2894DivisibleandNondivisibleSumsDifference.java
@@ -1,4 +1,6 @@
 /* 2894.[Easy] Divisible and Non-divisible Sums Difference */
 public class May27Problem2894DivisibleandNondivisibleSumsDifference {
-    
+    public static void main(String[] args) {
+        
+    }
 }
