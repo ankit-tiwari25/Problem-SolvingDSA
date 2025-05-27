39a3e6a596bbc85a8f9050056d5c66243d514bc2
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-05-27 18:44:56 +0530

    2894. Divisible and Non-divisible Sums Difference

--- May2025/May27Problem2894DivisibleandNondivisibleSumsDifference.java
+++ May2025/May27Problem2894DivisibleandNondivisibleSumsDifference.java
@@ -3,4 +3,12 @@ public class May27Problem2894DivisibleandNondivisibleSumsDifference {
     public static void main(String[] args) {
         
     }
+    public int differenceOfSums(int n, int m) {
+         int num1 = 0, num2 = 0;
+        for (int i = 1; i <= n; i++) {
+            if (i % m == 0) num2 += i;
+            else num1 += i;
+        }
+        return num1 - num2;
+    }
 }
