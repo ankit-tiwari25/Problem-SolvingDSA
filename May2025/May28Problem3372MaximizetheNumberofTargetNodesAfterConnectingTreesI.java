4727d15bb15f333f633be3ba760b91d7174e9188
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-05-28 20:03:53 +0530

    3372. Maximize the Number of Target Nodes After Connecting Trees I

--- May2025/May28Problem3372MaximizetheNumberofTargetNodesAfterConnectingTreesI.java
+++ May2025/May28Problem3372MaximizetheNumberofTargetNodesAfterConnectingTreesI.java
@@ -17,4 +17,12 @@ public class May28Problem3372MaximizetheNumberofTargetNodesAfterConnectingTreesI
         }
         return adj;
     }
+     private int dfs(List<List<Integer>> adj, int u, int p, int k) {
+        if (k < 0) return 0;
+        int cnt = 1;
+        for (int v : adj.get(u)) {
+            if (v != p) cnt += dfs(adj, v, u, k - 1);
+        }
+        return cnt;
+    }
 }
