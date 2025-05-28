488029a7bb59a9fba7846c36c000be62eca188c2
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-05-28 20:02:19 +0530

    3372. Maximize the Number of Target Nodes After Connecting Trees I

--- May2025/May28Problem3372MaximizetheNumberofTargetNodesAfterConnectingTreesI.java
+++ May2025/May28Problem3372MaximizetheNumberofTargetNodesAfterConnectingTreesI.java
@@ -1,4 +1,6 @@
 /* 3372.[Medium] Maximize the Number of Target Nodes After Connecting Trees I */
 public class May28Problem3372MaximizetheNumberofTargetNodesAfterConnectingTreesI {
-    
+    public static void main(String[] args) {
+        
+    }
 }
