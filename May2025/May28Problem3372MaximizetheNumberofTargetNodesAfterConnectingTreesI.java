4fda8361c1f37b3935fc47e283871ce4b7f9a81e
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-05-28 20:03:26 +0530

    3372. Maximize the Number of Target Nodes After Connecting Trees I

--- May2025/May28Problem3372MaximizetheNumberofTargetNodesAfterConnectingTreesI.java
+++ May2025/May28Problem3372MaximizetheNumberofTargetNodesAfterConnectingTreesI.java
@@ -1,6 +1,20 @@
 /* 3372.[Medium] Maximize the Number of Target Nodes After Connecting Trees I */
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class May28Problem3372MaximizetheNumberofTargetNodesAfterConnectingTreesI {
     public static void main(String[] args) {
         
+    }
+     private List<List<Integer>> buildList(int[][] edges) {
+        int n = edges.length + 1;
+        List<List<Integer>> adj = new ArrayList<>(n);
+        for (int i = 0; i < n; i++) adj.add(new ArrayList<>());
+        for (int[] e : edges) {
+            adj.get(e[0]).add(e[1]);
+            adj.get(e[1]).add(e[0]);
+        }
+        return adj;
     }
 }
