e054df73e702e3d6480c05d2bc80a423611bc632
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-05-28 20:08:58 +0530

    3372. Maximize the Number of Target Nodes After Connecting Trees I

--- May2025/May27Problem2894DivisibleandNondivisibleSumsDifference.java
+++ May2025/May27Problem2894DivisibleandNondivisibleSumsDifference.java
@@ -7,7 +7,7 @@ public class May27Problem2894DivisibleandNondivisibleSumsDifference {
     public static  int differenceOfSums(int n, int m) {
          int num1 = 0, num2 = 0;
         for (int i = 1; i <= n; i++) {
-            if (i % m == 0) num2 += i;
+            if (i % m == 0) num2 += i;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
             else num1 += i;
         }
         return num1 - num2;
