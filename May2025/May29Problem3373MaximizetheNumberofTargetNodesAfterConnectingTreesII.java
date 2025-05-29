eee6dfc125bf26dc29acd73e6d4aa267d8d559ad
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-05-29 20:09:39 +0530

    3373. Maximize the Number of Target Nodes After Connecting Trees II

--- May2025/May29Problem3373MaximizetheNumberofTargetNodesAfterConnectingTreesII.java
+++ May2025/May29Problem3373MaximizetheNumberofTargetNodesAfterConnectingTreesII.java
@@ -2,6 +2,7 @@
 /* 3373.[Hard] Maximize the Number of Target Nodes After Connecting Trees II */
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class May29Problem3373MaximizetheNumberofTargetNodesAfterConnectingTreesII {
@@ -30,5 +31,22 @@ public class May29Problem3373MaximizetheNumberofTargetNodesAfterConnectingTreesI
             color[v] = color[u] ^ 1;
             dfsColor(adj, v, u, color, isA);
         }
+    }
+     public int[] maxTargetNodes(int[][] edges1, int[][] edges2) {
+        List<List<Integer>> adjA = buildList(edges1), adjB = buildList(edges2);
+        int n = adjA.size(), m = adjB.size();
+        int[] colorA = new int[n], colorB = new int[m];
+        Arrays.fill(colorA, -1);
+        Arrays.fill(colorB, -1);
+        evenA = oddA = evenB = oddB = 0;
+        colorA[0] = 0;
+        dfsColor(adjA, 0, -1, colorA, true);
+        colorB[0] = 0;
+        dfsColor(adjB, 0, -1, colorB, false);
+        int maxiB = Math.max(evenB, oddB);
+        int[] res = new int[n];
+        for (int i = 0; i < n; i++)
+            res[i] = (colorA[i] == 0 ? evenA : oddA) + maxiB;
+        return res;
     }
 }
