d2d4509534a3af1d69828c166970f39d26b39091
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-05-30 21:07:20 +0530

    2359. Find Closest Node to Given Two Nodes

--- May2025/May30Problem2359FindClosestNodetoGivenTwoNodes.java
+++ May2025/May30Problem2359FindClosestNodetoGivenTwoNodes.java
@@ -1,6 +1,27 @@
 /* 2359.[Medium] Find Closest Node to Given Two Nodes */
+
+import java.util.Arrays;
+
 public class May30Problem2359FindClosestNodetoGivenTwoNodes {
     public static void main(String[] args) {
         
+    }
+     public int closestMeetingNode(int[] edges, int start1, int start2) {
+        int res = -1, Min_Of_Max = Integer.MAX_VALUE, n = edges.length;
+        int[] dist1 = new int[n], dist2 = new int[n];
+        Arrays.fill(dist1, -1);
+        Arrays.fill(dist2, -1);
+        dfs(start1, 0, edges, dist1);
+        dfs(start2, 0, edges, dist2);
+        for (int i = 0; i < n; i++) {
+            if (dist1[i] >= 0 && dist2[i] >= 0) {
+                int maxDist = Math.max(dist1[i], dist2[i]);
+                if (maxDist < Min_Of_Max) {
+                    Min_Of_Max = maxDist;
+                    res = i;
+                }
+            }
+        }
+        return res;
     }
 }
