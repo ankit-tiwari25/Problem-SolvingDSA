e40ab1fbaf6391da8591352a706c6ade9548a0cf
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-06-01 21:30:51 +0530

    2929. Distribute Candies Among Children II

--- June2025Challenge/Jun1Problem2929DistributeCandiesAmongChildrenII.java
+++ June2025Challenge/Jun1Problem2929DistributeCandiesAmongChildrenII.java
@@ -4,4 +4,15 @@ public class Jun1Problem2929DistributeCandiesAmongChildrenII {
   public static void main(String[] args) {
     
   }
+   public long distributeCandies(int n, int m) {
+          long res = ((long)n + 2) * (n + 1) / 2;
+        for (int i = 1; i <= 3; i++) {
+            long rem = n - (long)i * (m + 1);
+            if (rem < 0) break;
+            long val = (rem + 2) * (rem + 1) / 2;
+            long c = (i < 3 ? 3 : 1);
+            res += (i % 2 != 0 ? -c * val : c * val);
+        }
+        return res;
+    }
 }
