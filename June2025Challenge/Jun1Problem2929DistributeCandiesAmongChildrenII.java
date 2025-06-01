03abc48f9edff507142139b30410321d47dbae75
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-06-01 21:30:31 +0530

    2929. Distribute Candies Among Children II

--- June2025Challenge/Jun1Problem2929DistributeCandiesAmongChildrenII.java
+++ June2025Challenge/Jun1Problem2929DistributeCandiesAmongChildrenII.java
@@ -1,5 +1,7 @@
 package June2025Challenge;
 /* 2929.[Medium] Distribute Candies Among Children II */
 public class Jun1Problem2929DistributeCandiesAmongChildrenII {
+  public static void main(String[] args) {
     
+  }
 }
