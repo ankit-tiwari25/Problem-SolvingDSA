ddaef5350b0376dfefc8e175dd80cbaf9ec3d9d0
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-06-03 21:41:14 +0530

    1298. Maximum Candies You Can Get from Boxes

--- June2025Challenge/Jun03Problem1298MaximumCandiesYouCanGetfromBoxes.java
+++ June2025Challenge/Jun03Problem1298MaximumCandiesYouCanGetfromBoxes.java
@@ -1,7 +1,33 @@
 
 /* 1298.[Hard] Maximum Candies You Can Get from Boxes */
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Jun03Problem1298MaximumCandiesYouCanGetfromBoxes {
     public static void main(String[] args) {
         
+    }
+     public int maxCandies(int[] vis, int[] candies, int[][] keys, int[][] containedBoxes, int[] initialBoxes) {
+         boolean foundOpenable = true;
+        int totalCandies = 0;
+        List<Integer> boxes = new ArrayList<>();
+        for (int b : initialBoxes) boxes.add(b);
+        while (!boxes.isEmpty() && foundOpenable) {
+            foundOpenable = false;
+            List<Integer> nextBoxes = new ArrayList<>();
+            for (int boxId : boxes) {
+                if (vis[boxId] == 1) {
+                    foundOpenable = true;
+                    for (int k : containedBoxes[boxId]) nextBoxes.add(k);
+                    for (int keyId : keys[boxId]) vis[keyId] = 1;
+                    totalCandies += candies[boxId];
+                } else {
+                    nextBoxes.add(boxId);
+                }
+            }
+            boxes = nextBoxes;
+        }
+        return totalCandies;
     }
 }
