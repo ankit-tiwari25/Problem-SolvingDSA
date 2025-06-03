ec01b8bcde5a05b4b911ca9fe3cf86d0cddefe65
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-06-03 21:40:33 +0530

    1298. Maximum Candies You Can Get from Boxes

--- June2025Challenge/Jun03Problem1298MaximumCandiesYouCanGetfromBoxes.java
+++ June2025Challenge/Jun03Problem1298MaximumCandiesYouCanGetfromBoxes.java
@@ -1,5 +1,7 @@
 
 /* 1298.[Hard] Maximum Candies You Can Get from Boxes */
 public class Jun03Problem1298MaximumCandiesYouCanGetfromBoxes {
-    
+    public static void main(String[] args) {
+        
+    }
 }
