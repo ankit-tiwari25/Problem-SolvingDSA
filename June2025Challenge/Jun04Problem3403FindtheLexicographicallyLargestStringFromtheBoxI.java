e9cbc072b92fa1d0b397591044cb83a850446c46
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-06-04 20:09:45 +0530

    3403. Find the Lexicographically Largest String From the Box I

--- June2025Challenge/Jun04Problem3403FindtheLexicographicallyLargestStringFromtheBoxI.java
+++ June2025Challenge/Jun04Problem3403FindtheLexicographicallyLargestStringFromtheBoxI.java
@@ -5,4 +5,19 @@ public class Jun04Problem3403FindtheLexicographicallyLargestStringFromtheBoxI {
     public static void main(String[] args) {
         
     }
+    public String answerString(String word, int numFriends) {
+       if (numFriends == 1) return word;
+        String res = "";
+        int length = word.length() - numFriends + 1;
+        for (int i = 0; i < word.length(); i++) {
+            String temp;
+            if (i + length <= word.length())
+                temp = word.substring(i, i + length);
+            else
+                temp = word.substring(i);
+            if (temp.compareTo(res) > 0)
+                res = temp;
+        }
+        return res; 
+    }
 }
