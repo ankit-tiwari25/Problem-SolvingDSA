e763df05d117fef217c0e4b0c212b594bda98320
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-06-04 20:09:31 +0530

    3403. Find the Lexicographically Largest String From the Box I

--- June2025Challenge/Jun04Problem3403FindtheLexicographicallyLargestStringFromtheBoxI.java
+++ June2025Challenge/Jun04Problem3403FindtheLexicographicallyLargestStringFromtheBoxI.java
@@ -1,3 +1,5 @@
+package June2025Challenge;
+
 /* 3403.[Medium] Find the Lexicographically Largest String From the Box I */
 public class Jun04Problem3403FindtheLexicographicallyLargestStringFromtheBoxI {
     public static void main(String[] args) {
