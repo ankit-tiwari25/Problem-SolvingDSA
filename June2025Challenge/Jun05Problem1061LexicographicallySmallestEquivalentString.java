01486545ccc79abf959679dffce6208e082bf368
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-06-05 22:06:19 +0530

    1061. Lexicographically Smallest Equivalent String

--- June2025Challenge/Jun05Problem1061LexicographicallySmallestEquivalentString.java
+++ June2025Challenge/Jun05Problem1061LexicographicallySmallestEquivalentString.java
@@ -1,5 +1,5 @@
 package June2025Challenge;
-/* 1061. Lexicographically Smallest Equivalent String */
+/* 1061.[Medium] Lexicographically Smallest Equivalent String */
 public class Jun05Problem1061LexicographicallySmallestEquivalentString {
     
 }
