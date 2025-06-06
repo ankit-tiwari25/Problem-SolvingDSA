8de2d43c6134ea20433a40356eef04baba8a7f3d
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-06-06 22:03:27 +0530

    2434. Using a Robot to Print the Lexicographically Smallest String

--- June2025Challenge/Jun06Problem2434UsingaRobottoPrinttheLexicographicallySmallestString.java
+++ June2025Challenge/Jun06Problem2434UsingaRobottoPrinttheLexicographicallySmallestString.java
@@ -1,5 +1,7 @@
 package June2025Challenge;
 /* 2434.[Medium] Using a Robot to Print the Lexicographically Smallest String */
 public class Jun06Problem2434UsingaRobottoPrinttheLexicographicallySmallestString {
-    
+    public static void main(String[] args) {
+        
+    }
 }
