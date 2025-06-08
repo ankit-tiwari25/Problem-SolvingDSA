7c496c967bcf0c62ae5da234a0c8c2ab515f9b95
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-06-08 22:01:15 +0530

    386. Lexicographical Numbers

--- June2025Challenge/Jun08Problem386LexicographicalNumbers.java
+++ June2025Challenge/Jun08Problem386LexicographicalNumbers.java
@@ -1,5 +1,5 @@
 package June2025Challenge;
-
+/* 386.[Medium] Lexicographical Numbers */
 public class Jun08Problem386LexicographicalNumbers {
     public static void main(String[] args) {
         
