d3b121d5b0bbe58d091f858600dfada260b90c04
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-06-09 23:37:41 +0530

    440. K-th Smallest in Lexicographical Order

--- June2025Challenge/Jun09Problem440KthSmallestinLexicographicalOrder.java
+++ June2025Challenge/Jun09Problem440KthSmallestinLexicographicalOrder.java
@@ -4,4 +4,9 @@ public class Jun09Problem440KthSmallestinLexicographicalOrder {
     public static void main(String[] args) {
         
     }
+    static public int findKthNumber(int n, int K) {
+        long k = K;
+        int ans = solve(0, n, k);
+        return ans;
+    }
 }
