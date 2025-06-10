fd29d6b019cbe807a8532ef604616682adb79676
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-06-10 16:27:12 +0530

    3442. Maximum Difference Between Even and Odd Frequency I

--- June2025Challenge/Jun10Problem3442MaximumDifferenceBetweenEvenandOddFrequencyI.java
+++ June2025Challenge/Jun10Problem3442MaximumDifferenceBetweenEvenandOddFrequencyI.java
@@ -1,3 +1,5 @@
+
+/* 3442.[Easy] Maximum Difference Between Even and Odd Frequency I */
 public class Jun10Problem3442MaximumDifferenceBetweenEvenandOddFrequencyI {
     public static void main(String[] args) {
         
