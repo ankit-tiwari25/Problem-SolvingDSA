990f5b7acc7726948b5bab98f2b532692ff07576
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-06-14 01:52:31 +0530

    Sort a Stack using recursion

---
+++ Recursion/Retry/SortAStack.java
@@ -0,0 +1,13 @@
+package Recursion.Retry;
+
+import java.util.Stack;
+
+/* Sort a stack */
+public class SortAStack {
+    public static void main(String[] args) {
+        
+    }
+    public static Stack<Integer> sortAStack(Stack<Integer>){
+        
+    }
+}
