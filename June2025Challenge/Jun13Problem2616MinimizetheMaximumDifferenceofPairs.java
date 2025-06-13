a16cedac27b43b86b2c2f5acfb3b567fee60fdbf
ankit-tiwari25 <ankit.tiwari25@outlook.com>
2025-06-14 00:51:32 +0530

    2616. Minimize the Maximum Difference of Pairs

--- June2025Challenge/Jun13Problem2616MinimizetheMaximumDifferenceofPairs.java
+++ June2025Challenge/Jun13Problem2616MinimizetheMaximumDifferenceofPairs.java
@@ -1,4 +1,6 @@
 /* 2616.[Medium] Minimize the Maximum Difference of Pairs */
 public class Jun13Problem2616MinimizetheMaximumDifferenceofPairs {
-    
+    public static void main(String[] args) {
+        
+    }
 }
